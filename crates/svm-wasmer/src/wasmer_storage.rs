6c005c372132b7546664284d7d9614b700c41404
Yaron Wittenstein <yaron.wittenstein@gmail.com>
2019-07-09 16:40:54 +0300

    wasmer_storage.rs - `copy_to_wasmer_mem` started working too

--- crates/svm-wasmer/src/wasmer_storage.rs
+++ crates/svm-wasmer/src/wasmer_storage.rs
@@ -82,7 +82,7 @@ fn mem_to_reg_copy(ctx: &mut Ctx, src_mem_ptr: i32, dst_reg: i32, offset: i32, l
     reg.copy_from_wasmer_mem(cells);
 }
 
-fn reg_copy_to_mem_copy(ctx: &mut Ctx, src_reg: i32, dst_mem_ptr: i32, len: i32) {
+fn reg_to_mem_copy(ctx: &mut Ctx, src_reg: i32, dst_mem_ptr: i32, len: i32) {
     let reg = wasmer_reg!(ctx, src_reg);
     let cells = wasmer_mem_cells!(ctx, dst_mem_ptr, len);
 
@@ -115,7 +115,7 @@ mod tests {
     }
 
     #[test]
-    fn wasmer_reg_copy_from_wasmer_mem() {
+    fn reg_copy_from_wasmer_mem() {
         let regs = CtxRegs::new();
 
         let (data, _dtor) = state_creator(&regs);
@@ -124,7 +124,7 @@ mod tests {
         let reg0: &mut WasmerReg64 = ctx_regs_reg!(regs, 0);
         let reg1: &mut WasmerReg64 = ctx_regs_reg!(regs, 1);
 
-        // register `0` and `1` are initialized with zeros
+        // registers `0` and `1` are initialized with zeros
         assert_eq!(vec![0; 8], &reg0.0[..]);
         assert_eq!(vec![0; 8], &reg1.0[..]);
 
@@ -144,5 +144,47 @@ mod tests {
 
         assert_eq!(vec![10, 20, 30, 40, 50, 60, 70, 80], &reg0.0[..]);
         assert_eq!(vec![00, 00, 00, 00, 00, 00, 00, 00], &reg1.0[..]);
+    }
+
+    #[test]
+    fn reg_copy_to_wasmer_mem() {
+        let regs = CtxRegs::new();
+
+        let (data, _dtor) = state_creator(&regs);
+
+        let regs = wasmer_ctx_data_regs!(data);
+        let reg0: &mut WasmerReg64 = ctx_regs_reg!(regs, 0);
+
+        // initialize register `0` with data
+        let cells = vec![
+            Cell::new(10),
+            Cell::new(20),
+            Cell::new(30),
+            Cell::new(40),
+            Cell::new(50),
+            Cell::new(60),
+            Cell::new(70),
+            Cell::new(80),
+        ];
+
+        reg0.copy_from_wasmer_mem(&cells);
+        assert_eq!(vec![10, 20, 30, 40, 50, 60, 70, 80], &reg0.0[..]);
+
+        // copying register `0` to a fake wasmer memory at cells: `10, 11, ... 17` (inclusive)
+        let cells: Vec<Cell<u8>> = (0..8).map(|_| Cell::<u8>::new(0)).collect();
+
+        assert_eq!(
+            vec![00, 00, 00, 00, 00, 00, 00, 00],
+            cells.iter().map(|c| c.get()).collect::<Vec<u8>>()
+        );
+
+        // copying register `0` content into memory
+        reg0.copy_to_wasmer_mem(&cells);
+
+        // asserint that the fake wasmer memory has been changes
+        assert_eq!(
+            vec![10, 20, 30, 40, 50, 60, 70, 80],
+            cells.iter().map(|c| c.get()).collect::<Vec<u8>>()
+        );
     }
 }
