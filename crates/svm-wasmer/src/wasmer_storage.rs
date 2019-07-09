00a5ca8d7adcc1148a5c1997dcd64b2b91df8789
Yaron Wittenstein <yaron.wittenstein@gmail.com>
2019-07-09 16:08:18 +0300

    wasmer_storage.rs - copying data to registers started working :)

--- crates/svm-wasmer/src/wasmer_storage.rs
+++ crates/svm-wasmer/src/wasmer_storage.rs
@@ -24,16 +24,28 @@ macro_rules! ctx_regs_reg {
     ($regs: expr, $reg_idx: expr) => {{
         assert!($reg_idx >= 0 && $reg_idx < REGS_64_COUNT as i32);
 
-        &mut $regs.regs_64[$reg_idx as usize]
+        /// We don't do:
+        /// ```rust
+        /// let reg: &mut WasmerReg64 = $regs.regs_64[$reg_idx as usize];
+        /// ```
+        ///
+        /// Because we like to keep the option to  mutate a couple of registers simultaneously
+        /// without the Rust borrow checker getting angry...
+        /// so instead we use _Unsafe Rust_
+        let regs_ptr: *mut WasmerReg64 = unsafe { $regs.regs_64.as_mut_ptr() };
+
+        let reg_idx_ptr: *mut WasmerReg64 = unsafe { regs_ptr.offset($reg_idx as isize) };
+
+        let reg: &mut WasmerReg64 = unsafe { &mut *reg_idx_ptr };
+
+        reg
     }};
 }
 
 macro_rules! wasmer_ctx_data_regs {
     ($data: expr) => {{
         let data_ptr: *mut CtxRegs = unsafe { $data as *mut _ };
-
         let data: &mut CtxRegs = unsafe { &mut *data_ptr };
-
         data
     }};
 }
@@ -89,45 +101,48 @@ fn storage_set_from_reg(_ctx: &mut Ctx, page: i32, offset: i32, reg: i32) {
 mod tests {
     use super::*;
 
+    use std::cell::Cell;
     use std::ffi::c_void;
     use wasmer_runtime_core::{import::ImportObject, vm::Ctx};
 
-    fn allocate_ctx_regs() -> *mut c_void {
-        let mut ctx_regs = CtxRegs::new();
-
-        unsafe { &mut ctx_regs as *mut _ as *mut c_void }
-    }
-
     fn dtor(data: *mut c_void) {}
 
-    fn state_creator() -> (*mut c_void, fn(*mut c_void)) {
-        let data: *mut c_void = allocate_ctx_regs();
+    fn state_creator(regs: &CtxRegs) -> (*mut c_void, fn(*mut c_void)) {
+        let data: *mut c_void = unsafe { regs.clone() as *const _ as *mut c_void };
         let dtor: fn(*mut c_void) = |_| {};
 
         (data, dtor)
     }
 
     #[test]
-    fn init_ctx_regs() {
-        // let (data, _dtor) = state_creator();
-        //
-        // let regs = wasmer_ctx_data_regs!(data);
-        //
-        // let reg0: &mut WasmerReg64 = ctx_regs_reg!(regs, 0);
-        //
-        // dbg!(reg0);
+    fn wasmer_reg_copy_from_wasmer_mem() {
+        let regs = CtxRegs::new();
 
-        // let reg1 = reg0.clone();
-        // let reg2 = reg1;
-        // let reg3 = reg2;
-        //
-        // dbg!(reg0);
-        // dbg!(reg1);
-        // dbg!(reg3);
-        // dbg!(reg2);
+        let (data, _dtor) = state_creator(&regs);
 
-        // assert_eq!(&mut WasmerReg64::new(), reg0);
+        let regs = wasmer_ctx_data_regs!(data);
+        let reg0: &mut WasmerReg64 = ctx_regs_reg!(regs, 0);
+        let reg1: &mut WasmerReg64 = ctx_regs_reg!(regs, 1);
 
-        // let reg1 = ctx_regs_reg!(regs, 0);
+        // register `0` and `1` are initialized with zeros
+        assert_eq!(vec![0; 8], &reg0.0[..]);
+        assert_eq!(vec![0; 8], &reg1.0[..]);
+
+        // editing register `0` should not edit register `1`
+        let cells = vec![
+            Cell::new(10),
+            Cell::new(20),
+            Cell::new(30),
+            Cell::new(40),
+            Cell::new(50),
+            Cell::new(60),
+            Cell::new(70),
+            Cell::new(80),
+        ];
+
+        reg0.copy_from_wasmer_mem(&cells);
+
+        assert_eq!(vec![10, 20, 30, 40, 50, 60, 70, 80], &reg0.0[..]);
+        assert_eq!(vec![00, 00, 00, 00, 00, 00, 00, 00], &reg1.0[..]);
     }
 }
