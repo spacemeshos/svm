31ca1a0f63696c96be5a28b3a010a5e97a745670
Yaron Wittenstein <yaron.wittenstein@gmail.com>
2019-07-10 17:12:10 +0300

    svm-wasmer: * wasmer_register.rs - adding `get` / `set` methods

--- crates/svm-wasmer/src/wasmer_register.rs
+++ crates/svm-wasmer/src/wasmer_register.rs
@@ -46,6 +46,34 @@ macro_rules! impl_register {
                     cell.set(*byte);
                 }
             }
+
+            /// Returns a copy of the register content as a byte array
+            pub(crate) fn get(&self) -> [u8; $bytes_count] {
+                self.0
+            }
+
+            /// Overrides the content of the register with the input `bytes` (byte-array).
+            /// Pads remaining bytes with `0` in case `bytes` is small than the register capacity.
+            /// Panics when `bytes` is larger then the register capacity.
+            pub(crate) fn set(&mut self, bytes: &[u8]) {
+                let padding = $bytes_count as isize - bytes.len() as isize;
+
+                if padding > 0 {
+                    for (i, byte) in bytes.iter().enumerate() {
+                        self.0[i] = *byte;
+                    }
+
+                    // zeroing the remaining bytes
+                    for i in bytes.len()..$bytes_count {
+                        self.0[i] = 0;
+                    }
+                } else if padding == 0 {
+                    // optimized `set`
+                    self.0.copy_from_slice(&bytes);
+                } else {
+                    panic!("`bytes` can't fit register");
+                }
+            }
         }
 
         impl Debug for $reg_ident {
@@ -63,3 +91,44 @@ macro_rules! impl_register {
 }
 
 impl_register!(8, WasmerReg64);
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn get_defaults_to_zeros() {
+        let mut reg = WasmerReg64::new();
+
+        assert_eq!([0; 8], reg.get());
+    }
+
+    #[test]
+    fn set_exact_register_bytes_size() {
+        let mut reg = WasmerReg64::new();
+        assert_eq!([0; 8], reg.get());
+
+        reg.set(&vec![10, 20, 30, 40, 50, 60, 70, 80]);
+
+        assert_eq!([10, 20, 30, 40, 50, 60, 70, 80], reg.get());
+    }
+
+    #[test]
+    fn set_less_than_register_capacity() {
+        let mut reg = WasmerReg64::new();
+        reg.set(&vec![10; 8]);
+        assert_eq!([10; 8], reg.get());
+
+        // now we `set` less than register bytes on register `0` (which already has data in it)
+        reg.set(&vec![20, 30, 40]);
+
+        assert_eq!([20, 30, 40, 0, 0, 0, 0, 0], reg.get());
+    }
+
+    // #[test]
+    // #[should_panic(expected = "`bytes` can't fit register")]
+    // fn setting_data_larger_than_register_capacity_raises() {
+    //     let mut reg = WasmerReg64::new();
+    //     reg.set(&vec![10; 9]);
+    // }
+}
