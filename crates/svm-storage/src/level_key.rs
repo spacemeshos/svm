43f19f26e9e2d6a03dc476a46b6a7c3bc27894a4
Yaron Wittenstein <yaron.wittenstein@gmail.com>
2019-07-16 14:33:00 +0300

    svm-storage: * implementing `db_key::Key` trait for the leveldb integration

---
+++ crates/svm-storage/src/level_key.rs
@@ -0,0 +1,48 @@
+macro_rules! impl_leveldb_key {
+    ($key_name: ident, $bytes_count: expr) => {
+        /// Implements `db_key::Key` trait. The inner data is an array of $bytes_count bytes
+        #[derive(Debug, Copy, Clone, Hash, PartialEq, Eq, PartialOrd, Ord)]
+        pub struct $key_name([u8; $bytes_count]);
+
+        impl db_key::Key for $key_name {
+            fn from_u8(key: &[u8]) -> Self {
+                assert!(key.len() == $bytes_count);
+
+                let mut bytes = [0; $bytes_count];
+                bytes.copy_from_slice(key);
+
+                Self(bytes)
+            }
+
+            fn as_slice<T, F: Fn(&[u8]) -> T>(&self, f: F) -> T {
+                f(&self.0)
+            }
+        }
+    };
+}
+
+impl_leveldb_key!(LevelKey32, 32);
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use db_key::Key;
+
+    impl_leveldb_key!(LevelKeyTest3, 3);
+
+    #[test]
+    fn from_u8() {
+        let key: LevelKeyTest3 = LevelKeyTest3::from_u8(&[10, 20, 30]);
+
+        assert_eq!([10, 20, 30], key.0);
+    }
+
+    #[test]
+    fn as_slice() {
+        let key: LevelKeyTest3 = LevelKeyTest3::from_u8(&[10, 20, 30]);
+
+        let data: Vec<u8> = key.as_slice(|inner: &[u8]| inner.to_vec());
+
+        assert_eq!(vec![10, 20, 30], data);
+    }
+}
--- crates/svm-storage/src/lib.rs
+++ crates/svm-storage/src/lib.rs
@@ -8,7 +8,7 @@ mod default_page_hasher;
 mod default_pages_storage;
 
 #[cfg(feature = "leveldb_kv")]
-mod leveldb_kv;
+mod level_key;
 
 #[cfg(feature = "memory_kv")]
 mod mem_kv_store;
@@ -36,8 +36,10 @@ use default_page_hasher::DefaultPageHasher;
 use default_pages_storage::DefaultPagesStorage;
 
 #[cfg(feature = "leveldb_kv")]
-pub use leveldb_kv::LevelDB;
+pub use level_key::*;
 
+// #[cfg(feature = "leveldb_kv")]
+//  pub use leveldb_kv::LevelDB;
 #[cfg(feature = "memory_kv")]
 pub use mem_kv_store::MemKVStore;
 
