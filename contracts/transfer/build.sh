f9765e65c121836f9618033108f9879ffdd5c01b
Yaron Wittenstein <yaron.wittenstein@gmail.com>
2019-07-22 14:50:07 +0300

    `contracts/transfer` - `build.sh` fix (output to `precompiled` directory)

--- contracts/transfer/build.sh
+++ contracts/transfer/build.sh
@@ -1,5 +1,5 @@
 cargo +nightly build --target wasm32-unknown-unknown --release
-cp target/wasm32-unknown-unknown/release/*.wasm ../../contracts/wasm
-pushd ../wasm
+cp target/wasm32-unknown-unknown/release/*.wasm ../../contracts/precompiled
+pushd ../precompiled
 wapm run wasm2wat transfer.wasm > transfer.wast
 popd
