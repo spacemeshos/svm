c597a128130b37a88f01d2ca7b0f27cee81c0eaf
Yaron Wittenstein <yaron.wittenstein@gmail.com>
2019-08-20 17:32:27 +0300

    svm-wasmer: * added more planning to implementing `runtime.rs`

--- crates/svm-wasmer/src/runtime.rs
+++ crates/svm-wasmer/src/runtime.rs
@@ -12,24 +12,36 @@ macro_rules! include_svm_runtime {
             svm_contract::build_wasm_contract::<$CONTRACT_TYPES>(&bytes)
         }
 
+        #[inline(always)]
+        pub fn contract_validate(contract: &WasmContract) -> Result<WasmContract, ContractError> {
+            // 1. validates the `wasm`
+            // 2. computes contract's address for contract using `CONTRACT_TYPES::AddressCompute` (implements `ContractAddressCompute`)
+            unimplemented!()
+        }
+
         pub fn contract_store(contract: &WasmContract) {
+            // 1. Looks if contract exists under `CONTRACT_TYPES::Store` (import `CodeHashStore`)
+            // 2. If contract exists, panics
+            // 3. Else, stores contract under `CONTRACT_TYPES::Store`
             unimplemented!()
         }
 
         pub fn contract_exec(tx: Tx) {
-            // 1. load contract wasmer module `tx.Address`
+            // 1. Load contract wasmer module `tx.Address`
             //  * if it's NOT in the compiled-modules-cache
             //      * Gets the wasm code from the `CONTRACT_TYPES::Store` (implements `CodeHashStore`)
-            //      * Compiles the module
+            //      * Compile the module using `svm_compiler::compile_program(..)`
             //      * Store into the compiled-modules-cache
             //
-            // 2. build the import object with `address = tx.Address` and `state = tx.State`
+            // 2. Validates that module has function `tx.FuncName` and that it can accept `tx.FuncArgs`
             //
-            // 3. instantiate wasm instance
+            // 3. Build the import object with `address = tx.Address` and `state = tx.State`
             //
-            // 4. Get the exported function `tx.FuncName`
+            // 4. Instantiate wasm instance
             //
-            // 5. execute the function with input `tx.FuncArgs`
+            // 5. Get the exported function `tx.FuncName`
+            //
+            // 6. Execute the function with input `tx.FuncArgs`
         }
     }};
 }
