84d5ad30b8aaea16bd25eecd9fb3684e412ca6f5
Yaron Wittenstein <yaron.wittenstein@gmail.com>
2019-09-23 15:07:44 +0300

    workspace `lib.rs` fix

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 extern crate svm_common;
 extern crate svm_compiler;
 extern crate svm_contract;
+extern crate svm_runtime;
+extern crate svm_runtime_c_api;
 extern crate svm_storage;
-extern crate svm_wasmer;
-extern crate svm_wasmer_c_api;
