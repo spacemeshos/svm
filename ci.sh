e48c2f29199ce950b11c26dd36287401aad8a404
Jorropo <jorropo.pgm@gmail.com>
2019-11-06 20:03:07 +0100

    Cleaner config

--- ci.sh
+++ ci.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# Config option
+
+# Test thread, see README.md to see why:
+export RUST_TEST_THREADS=1
+# Options (Don't include --release, that added for build needing it):
+export OPT="-p svm-runtime-c-api"
+# Extra windows opt
+export EXTRAOPT="--target x86_64-pc-windows-gnu"
+
 # This function aim to interupt the build if a step fail.
 check()
 {
@@ -18,19 +27,19 @@ installCross()
 # Debug and Release are the default *nix friendly ci (used by linux and osx build).
 # Windows* use docker and cross to build and test in a windows docker in linux.
 if [ "$SUITE" == "debug" ]; then
-	cargo +nightly build -p svm-runtime-c-api ; check
-	RUST_TEST_THREADS=1 cargo +nightly test -p svm-runtime-c-api ; check
+	cargo +nightly build $OPT ; check
+	cargo +nightly test $OPT ; check
 elif [ "$SUITE" == "release" ]; then
-  cargo +nightly build -p svm-runtime-c-api --release ; check
-  RUST_TEST_THREADS=1 cargo +nightly test -p svm-runtime-c-api --release ; check
+  cargo +nightly build --release $OPT ; check
+  cargo +nightly test --release $OPT ; check
 elif [ "$SUITE" == "windowsDebug" ]; then
 	installCross
-	cross +nightly build --all --target x86_64-pc-windows-gnu ; check
-	RUST_TEST_THREADS=1 cross +nightly test --all --target x86_64-pc-windows-gnu ; check
+	cross +nightly build $EXTRAOPT $OPT ; check
+	cross +nightly test $EXTRAOPT $OPT ; check
 elif [ "$SUITE" == "windowsRelease" ]; then
 	installCross
-	cross +nightly build --all --release --target x86_64-pc-windows-gnu ; check
-  RUST_TEST_THREADS=1 cross +nightly test --all --release --target x86_64-pc-windows-gnu ; check
+	cross +nighstly build --release $EXTRAOPT $OPT; check
+  cross +nightly test --release $EXTRAOPT $OPT; check
 elif [ "$SUITE" == "lint" ]; then
 	# Install rustfmt
 	rustup component add rustfmt --toolchain nightly ; check
