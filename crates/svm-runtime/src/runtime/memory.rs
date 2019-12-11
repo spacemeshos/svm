53759877c4e9aac4f77b414db5fda41ed0bdf834
Yaron Wittenstein <yaron.wittenstein@gmail.com>
2019-12-11 14:38:16 +0200

    WIP

--- crates/svm-runtime/src/runtime/memory.rs
+++ crates/svm-runtime/src/runtime/memory.rs
@@ -1,6 +1,3 @@
-use crate::opts::Opts;
-use crate::runtime::Runtime;
-
 use std::cell::RefCell;
 use std::rc::Rc;
 
@@ -17,22 +14,25 @@ use svm_storage::{
     ContractStorage,
 };
 
+use crate::opts::Opts;
+use crate::runtime::Runtime;
+
 pub fn create_memory_runtime(addr: Address, state: State, opts: &Opts) -> Runtime<MemoryEnv> {
-    let env_builder = Box::new(memory_runtime_env_build);
-    let storage_builder = Box::new(memory_runtime_storage_build);
+    let env_builder = Box::new(runtime_contract_env_build);
+    let storage_builder = Box::new(runtime_contract_storage_build);
 
     Runtime::new(env_builder, storage_builder)
 }
 
-fn memory_runtime_env_build(_path: &str) -> MemoryEnv {
+fn runtime_contract_env_build(_path: &str) -> MemoryEnv {
     let store = MemContractStore::new();
     MemoryEnv::new(store)
 }
 
-fn memory_runtime_storage_build(addr: Address, state: State, opts: &Opts) -> ContractStorage {
+fn runtime_contract_storage_build(addr: Address, state: State, opts: &Opts) -> ContractStorage {
     let kv = Rc::new(RefCell::new(MemKVStore::new()));
     let pages = MemContractPages::new(addr, kv, state, opts.max_pages as u32);
     let page_cache = MemContractPageCache::new(pages, opts.max_pages);
-    let storage = ContractStorage::new(Box::new(page_cache));
-    storage
+
+    ContractStorage::new(Box::new(page_cache))
 }
--- crates/svm-runtime/src/runtime/rocksdb.rs
+++ crates/svm-runtime/src/runtime/rocksdb.rs
@@ -1,9 +1,7 @@
 use std::cell::RefCell;
-use std::ffi::c_void;
 use std::path::Path;
 use std::rc::Rc;
 
-use crate::opts::Opts;
 use svm_common::{Address, State};
 use svm_kv::rocksdb::Rocksdb;
 
@@ -11,24 +9,23 @@ use svm_contract::rocksdb::{RocksdbContractEnv, RocksdbContractStore};
 use svm_storage::rocksdb::{RocksdbContractPageCache, RocksdbContractPages};
 use svm_storage::ContractStorage;
 
+use crate::opts::Opts;
 use crate::runtime::Runtime;
 
-use svm_contract::wasm::{WasmContractJsonDeserializer as D, WasmContractJsonSerializer as S};
-
 pub fn create_rocksdb_runtime(path: &str) -> Runtime<RocksdbContractEnv> {
-    let env_builder = Box::new(rocksdb_runtime_env_build);
-    let storage_builder = Box::new(rocksdb_runtime_storage_build);
+    let env_builder = Box::new(runtime_contract_env_build);
+    let storage_builder = Box::new(runtime_contract_storage_build);
 
     Runtime::new(env_builder, storage_builder)
 }
 
-pub fn rocksdb_runtime_env_build(path: &str) -> RocksdbContractEnv {
+pub fn runtime_contract_env_build(path: &str) -> RocksdbContractEnv {
     let path = Path::new(path);
     let store = RocksdbContractStore::new(path);
     RocksdbContractEnv::new(store)
 }
 
-fn rocksdb_runtime_storage_build(addr: Address, state: State, opts: &Opts) -> ContractStorage {
+fn runtime_contract_storage_build(addr: Address, state: State, opts: &Opts) -> ContractStorage {
     let path = Path::new(&opts.kv_path);
     let kv = Rc::new(RefCell::new(Rocksdb::new(path)));
 
