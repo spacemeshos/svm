e4fc810ceec95a0adee74414c47b65573a9ea915
Yaron Wittenstein <yaron.wittenstein@gmail.com>
2019-12-24 15:48:20 +0200

    moving tests helpers under `src/testing.rs`

--- crates/svm-runtime-c-api/src/testing.rs
+++ crates/svm-runtime-c-api/src/testing.rs
@@ -1,7 +1,19 @@
 use std::ffi::c_void;
+use std::sync::Arc;
 
-use wasmer_runtime_c_api::instance::wasmer_instance_context_t;
-use wasmer_runtime_core::vm::Ctx;
+use wasmer_runtime_c_api::{
+    export::{wasmer_import_export_kind, wasmer_import_export_value},
+    import::{wasmer_import_func_new, wasmer_import_func_t, wasmer_import_t},
+    instance::wasmer_instance_context_t,
+    value::wasmer_value_tag,
+    wasmer_byte_array, wasmer_result_t,
+};
+
+use wasmer_runtime_core::{
+    export::{Context, Export, FuncPointer},
+    types::{FuncSig, Type},
+    vm::{Ctx, Func},
+};
 
 use svm_runtime::ctx::SvmCtx;
 use svm_runtime::helpers::cast_ptr_to_svm_ctx;
@@ -23,14 +35,69 @@ pub unsafe fn svm_host_get<'a, T>(raw_ctx: *mut wasmer_instance_context_t) -> &'
     &mut *(svm_ctx.host as *mut T)
 }
 
-pub unsafe fn alloc_ptr() -> *mut c_void {
-    let ptr = std::ptr::null_mut();
-    let ptr: Box<*mut c_void> = Box::new(ptr);
-
-    let ptr: *mut *mut c_void = Box::into_raw(ptr);
-    *ptr
+pub unsafe fn cast_to_wasmer_ctx<'a>(ctx: *mut wasmer_instance_context_t) -> &'a mut Ctx {
+    &mut *(ctx as *mut Ctx)
 }
 
-unsafe fn cast_to_wasmer_ctx<'a>(ctx: *mut wasmer_instance_context_t) -> &'a mut Ctx {
-    &mut *(ctx as *mut Ctx)
+pub unsafe fn alloc_ptr() -> *mut c_void {
+    let ptr: *mut c_void = std::ptr::null_mut();
+    let ptr = Box::new(ptr);
+
+    *Box::into_raw(ptr)
+}
+
+pub unsafe fn wasmer_import_func_build(
+    func: *mut c_void,
+    params: Vec<Type>,
+    returns: Vec<Type>,
+) -> *mut wasmer_import_func_t {
+    let func: *const Func = &Func(func) as _;
+
+    let export = Export::Function {
+        func: FuncPointer::new(func),
+        ctx: Context::Internal,
+        signature: Arc::new(FuncSig::new(params, returns)),
+    };
+
+    Box::into_raw(Box::new(export)) as _
+}
+
+pub unsafe fn wasmer_import_func_destroy(func: *mut wasmer_import_func_t) {
+    Box::from_raw(func);
+}
+
+pub fn str_to_bytes(s: &str) -> (*const u8, u32) {
+    let bytes = s.as_ptr();
+    let bytes_len = s.len() as u32;
+
+    (bytes, bytes_len)
+}
+
+pub unsafe fn wasmer_import_func_create(
+    module_name: &str,
+    import_name: &str,
+    func: *mut c_void,
+    params: Vec<Type>,
+    returns: Vec<Type>,
+) -> wasmer_import_t {
+    let module_name = str_to_wasmer_byte_array(module_name);
+    let import_name = str_to_wasmer_byte_array(import_name);
+    let func = wasmer_import_func_build(func, params, returns);
+
+    wasmer_import_t {
+        module_name,
+        import_name,
+        tag: wasmer_import_export_kind::WASM_FUNCTION,
+        value: wasmer_import_as_value(func),
+    }
+}
+
+fn str_to_wasmer_byte_array(s: &str) -> wasmer_byte_array {
+    let (bytes, bytes_len) = str_to_bytes(s);
+
+    wasmer_byte_array { bytes, bytes_len }
+}
+
+fn wasmer_import_as_value(func: *const wasmer_import_func_t) -> wasmer_import_export_value {
+    wasmer_import_export_value { func }
 }
--- crates/svm-runtime-c-api/tests/api_tests.rs
+++ crates/svm-runtime-c-api/tests/api_tests.rs
@@ -3,10 +3,10 @@
 extern crate svm_runtime_c_api;
 
 use svm_runtime_c_api as api;
+use svm_runtime_c_api::testing;
 
 use std::collections::HashMap;
 use std::ffi::c_void;
-use std::sync::Arc;
 
 use wasmer_runtime_c_api::{
     export::{wasmer_import_export_kind, wasmer_import_export_value},
@@ -52,69 +52,6 @@ extern "C" fn vmcall_get_balance(
     0
 }
 
-unsafe fn wasmer_import_func_build(
-    func: *mut c_void,
-    params: Vec<Type>,
-    returns: Vec<Type>,
-) -> *mut wasmer_import_func_t {
-    let func: *const Func = &Func(func) as _;
-
-    let export = Export::Function {
-        func: FuncPointer::new(func),
-        ctx: Context::Internal,
-        signature: Arc::new(FuncSig::new(params, returns)),
-    };
-
-    Box::into_raw(Box::new(export)) as _
-}
-
-unsafe fn wasmer_import_func_destroy(func: *mut wasmer_import_func_t) {
-    Box::from_raw(func);
-}
-
-fn str_to_wasmer_byte_array(s: &str) -> wasmer_byte_array {
-    let (bytes, bytes_len) = str_to_bytes(s);
-
-    wasmer_byte_array { bytes, bytes_len }
-}
-
-fn str_to_bytes(s: &str) -> (*const u8, u32) {
-    let bytes = s.as_ptr();
-    let bytes_len = s.len() as u32;
-
-    (bytes, bytes_len)
-}
-
-fn wasmer_import_as_value(func: *const wasmer_import_func_t) -> wasmer_import_export_value {
-    wasmer_import_export_value { func }
-}
-
-unsafe fn wasmer_import_func_create(
-    module_name: &str,
-    import_name: &str,
-    func: *mut c_void,
-    params: Vec<Type>,
-    returns: Vec<Type>,
-) -> wasmer_import_t {
-    let module_name = str_to_wasmer_byte_array(module_name);
-    let import_name = str_to_wasmer_byte_array(import_name);
-    let func = wasmer_import_func_build(func, params, returns);
-
-    wasmer_import_t {
-        module_name,
-        import_name,
-        tag: wasmer_import_export_kind::WASM_FUNCTION,
-        value: wasmer_import_as_value(func),
-    }
-}
-
-unsafe fn alloc_ptr() -> *mut c_void {
-    let ptr: *mut c_void = std::ptr::null_mut();
-    let ptr = Box::new(ptr);
-
-    *Box::into_raw(ptr)
-}
-
 #[test]
 fn sanity() {
     unsafe {
@@ -123,7 +60,7 @@ fn sanity() {
 }
 
 unsafe fn unsafe_sanity() {
-    let get_balance_import = wasmer_import_func_create(
+    let get_balance_import = testing::wasmer_import_func_create(
         "env",
         "get_balance",
         &mut vmcall_get_balance as *mut _ as _,
@@ -134,8 +71,8 @@ unsafe fn unsafe_sanity() {
     let mut host = Host::new();
     let host_ptr: *mut c_void = &mut host as *mut Host as _;
 
-    let mut runtime = alloc_ptr();
-    let (path_bytes, path_len) = str_to_bytes("tests");
+    let mut runtime = testing::alloc_ptr();
+    let (path_bytes, path_len) = testing::str_to_bytes("tests");
 
     let imports = std::ptr::null_mut();
     let imports_len = 0;
