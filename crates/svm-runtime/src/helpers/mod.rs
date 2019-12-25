c121a513e5e4cd5162f623447ad89d1617a95801
Yaron Wittenstein <yaron.wittenstein@gmail.com>
2019-12-25 21:06:27 +0200

    svm-runtime: removing `helpers/ctx.rs`

--- crates/svm-runtime/src/helpers/mod.rs
+++ crates/svm-runtime/src/helpers/mod.rs
@@ -1,11 +1,9 @@
 #[macro_use]
 mod register;
 
-mod ctx;
 mod ptr_wrapper;
 mod storage;
 
-pub use ctx::cast_ptr_to_svm_ctx;
 pub use ptr_wrapper::PtrWrapper;
 pub use register::wasmer_data_reg;
 pub use storage::wasmer_data_contract_storage;
--- crates/svm-runtime/src/helpers/storage.rs
+++ crates/svm-runtime/src/helpers/storage.rs
@@ -1,12 +1,12 @@
 use std::ffi::c_void;
 
-use crate::helpers;
+use crate::ctx::SvmCtx;
 
 use svm_storage::ContractStorage;
 
 /// Extracts the `wasmer` instance context `data` field (of type `*mut c_void`) into `&mut ContractStorage`.
 #[inline(always)]
-pub fn wasmer_data_contract_storage<'a>(data: *const c_void) -> &'a mut ContractStorage {
-    let svm_ctx = helpers::cast_ptr_to_svm_ctx(data);
+pub fn wasmer_data_contract_storage<'a>(data: *mut c_void) -> &'a mut ContractStorage {
+    let svm_ctx = unsafe { svm_common::from_raw_mut::<SvmCtx>(data) };
     &mut svm_ctx.storage
 }
