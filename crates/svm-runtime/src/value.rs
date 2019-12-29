cabdf17de35be45dd4151a612c2ec03c0c2dbe8a
Yaron Wittenstein <yaron.wittenstein@gmail.com>
2019-12-29 11:23:01 +0200

    value.rs - adding tests for `TryFrom`

--- crates/svm-runtime/src/value.rs
+++ crates/svm-runtime/src/value.rs
@@ -8,8 +8,9 @@ pub enum Value {
     I64(i64),
 }
 
+#[derive(Debug, PartialEq)]
 pub enum ValueCastError {
-    NotSupportType(&'static str),
+    NotSupportedType(&'static str),
 }
 
 impl TryFrom<WasmerValue> for Value {
@@ -19,9 +20,41 @@ impl TryFrom<WasmerValue> for Value {
         match value {
             WasmerValue::I32(v) => Ok(Value::I32(v)),
             WasmerValue::I64(v) => Ok(Value::I64(v)),
-            WasmerValue::F32(_) => Err(ValueCastError::NotSupportType("F32")),
-            WasmerValue::F64(_) => Err(ValueCastError::NotSupportType("F64")),
-            WasmerValue::V128(_) => Err(ValueCastError::NotSupportType("V128")),
+            WasmerValue::F32(_) => Err(ValueCastError::NotSupportedType("F32")),
+            WasmerValue::F64(_) => Err(ValueCastError::NotSupportedType("F64")),
+            WasmerValue::V128(_) => Err(ValueCastError::NotSupportedType("V128")),
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn value_try_from() {
+        let wasmer_value = WasmerValue::I32(10);
+        assert_eq!(Ok(Value::I32(10)), Value::try_from(wasmer_value));
+
+        let wasmer_value = WasmerValue::I64(20);
+        assert_eq!(Ok(Value::I64(20)), Value::try_from(wasmer_value));
+
+        let wasmer_value = WasmerValue::F32(10.0);
+        assert_eq!(
+            Err(ValueCastError::NotSupportedType("F32")),
+            Value::try_from(wasmer_value)
+        );
+
+        let wasmer_value = WasmerValue::F64(20.0);
+        assert_eq!(
+            Err(ValueCastError::NotSupportedType("F64")),
+            Value::try_from(wasmer_value)
+        );
+
+        let wasmer_value = WasmerValue::V128(0);
+        assert_eq!(
+            Err(ValueCastError::NotSupportedType("V128")),
+            Value::try_from(wasmer_value)
+        );
     }
 }
