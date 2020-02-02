c24ea6b458a3a957ab14442bfaf05b45e2d44031
Yaron Wittenstein <yaron.wittenstein@gmail.com>
2020-02-02 16:56:46 +0200

    WIP

--- crates/svm-runtime-c-api/examples/counter.c
+++ crates/svm-runtime-c-api/examples/counter.c
@@ -177,24 +177,6 @@ svm_byte_array spawn_app_bytes(svm_byte_array template_addr) {
   return app;
 }
 
-uint8_t* int32_arg_new(uint32_t value) {
-  // we allocate 5 bytes for `int32` arg:
-  // 1 - arg type
-  // 4 - encoding of `value` (big-endian)
-  uint8_t* buf = (uint8_t*)malloc(5);
-
-  // arg type i32 = 0
-  buf[0] = 0;
-
-  // `value` is assumed to be laid-out in *little-endian* in memory
-  buf[1] = (value >> 24) & 0xFF;
-  buf[2] = (value >> 16) & 0xFF;
-  buf[3] = (value >> 8) & 0xFF;
-  buf[4] = (value >> 0) & 0xFF;
-
-  return buf;
-}
-
 svm_byte_array host_ctx_empty_bytes() {
   uint32_t length =
     4  +  // proto version
@@ -225,10 +207,8 @@ svm_byte_array exec_app_bytes(
     svm_func_buf_t func_buf,
     svm_func_args_t func_args
 ) {
-  svm_byte_array tx;
-
   uint32_t length =
-    4   +   // proto version
+    4   +  // proto version
     20  +  // app address
     1   +  // function name length
     func_name.length +  
@@ -236,15 +216,93 @@ svm_byte_array exec_app_bytes(
     func_args_length(func_args);
 
   uint8_t* buf = (uint8_t*)(malloc(length));  
+  uint32_t cursor = 0;
 
   // set `proto=0`
-  /* buf[0] = 0; */
-  /* buf[1] = 0; */
-  /* buf[2] = 0; */
-  /* buf[3] = 0; */
+  buf[0] = 0;
+  buf[1] = 0;
+  buf[2] = 0;
+  buf[3] = 0;
+  cursor += 4;
 
   // set `app` address
-  /* memcpy(&buf[4], app_addr, 20); */
+  memcpy(&buf[cursor], app_addr, 20);
+  cursor += 20;
+  
+  // `name length` consumes 1 byte
+  buf[cursor] = 1; 
+  cursor += 1;
+
+  // set `name length`
+  memcpy(&buf[cursor], func_name.bytes, func_name.length);
+  cursor += func_name.length;
+
+  // function buf
+
+  //// `func buf #slices` conumes 1 byte
+  buf[cursor] = 1; 
+  cursor += 1;
+
+  for (uint8_t i = 0; i < func_buf.slice_count; i++) {
+    svm_byte_array slice = func_buf.slices[i];
+
+    assert(slice.length <= 0xFFFF);
+
+    //// we assume `slice_len` is laid out in Little-Endian in memory.
+    uint16_t slice_len = (uint16_t)slice.length; 
+
+    //// slice length consumes 2 bytes (Big-Endian)
+    buf[cursor + 0] = (uint8_t)((slice_len >> 8) & 0xFF);
+    buf[cursor + 1] = (uint8_t)((slice_len >> 0) & 0xFF); 
+    cursor += 2;
+
+    //// copy slice to `buf`
+    memcpy(&buf[cursor], slice.bytes, slice.length);
+    cursor += slice.length;
+  }
+
+  // function args
+
+  //// `func #args` consumes 1 byte
+  buf[cursor] = 1; 
+  cursor += 1;
+
+  //// copy `func args` to `buf`
+  for (uint8_t i = 0; i < func_args.arg_count; i++) {
+    svm_func_arg_t arg = func_args.args[i];
+
+    svm_value_type arg_type = arg.type;
+
+    //// arg type consumes 1 byte
+    buf[cursor] = (uint8_t)arg_type;
+    cursor += 1;
+
+    if (arg_type == SVM_I32) {
+      for (uint8_t off = 0; off < 4; off++) {
+	buf[cursor + off] = *(arg.bytes + 3 - off); 
+      }
+
+      cursor += 4; //// arg value takes 4 bytes
+    }
+    else if (arg_type == SVM_I64) {
+      for (uint8_t off = 0; off < 8; off++) {
+	buf[cursor + off] = *(arg.bytes + 7 - off); 
+      }
+      
+      cursor += 8; //// arg value takes 8 bytes
+    }
+    else {
+      //// ilegal argument type
+      exit(1);
+    }
+  }
+
+  assert(cursor == length);
+
+  svm_byte_array tx = {
+    .length = length,
+    .bytes = buf
+  };
 
   return tx;
 }
