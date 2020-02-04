fc32ad6b7f65a227542d44cef8e66259865a340a
Yaron Wittenstein <yaron.wittenstein@gmail.com>
2020-02-04 16:39:32 +0200

    Sketching a packed `exec-app` raw format.

--- crates/svm-app/src/raw/transaction/mod.rs
+++ crates/svm-app/src/raw/transaction/mod.rs
@@ -1,23 +1,20 @@
-//!    Execute `AppTransaction` Raw Format Version 0.0.0.0
+//!    Execute `AppTransaction` Raw Format Version 0.0
 //!  -------------------------------------------------------
 //!  |   proto    |                                        |
 //!  |  version   |             `AppAddress`               |
-//!  |  (4 bytes) |              (20 bytes)                |
+//!  |  (2 bytes) |              (20 bytes)                |
 //!  |____________|________________________________________|
-//!  |             |                                       |
-//!  |  func name  |                                       |
-//!  |   length    |          func name (UTF-8)            |
-//!  |  (1 byte)   |                                       |
-//!  |_____________|_______________________________________|
-//!  |  func-buf  |  func-buf  |  func-buf  |              |
-//!  |   #slices  |   slice #1 |  slice #1  |              |
-//!  |            |    len     |            |   . . . .    |
-//!  | (1 byte)   | (2 bytes)  |   value    |              |
-//!  |____________|____________|____________|______________|
-//!  | func-args |    func   |    func   |                 |
-//!  |   #args   |   arg #1  |   arg #1  |                 |
-//!  |           |    type   |   value   |     . . . .     |
-//!  | (1 byte)  | (1 byte)  | (u32/u64) |                 |
+//!  |                                                     |
+//!  |        func index (2 bytes, Big-Endian)             |
+//!  |_____________________________________________________|
+//!  |  func-buf  |                                        |
+//!  |  #length   |           func-buf blob                |
+//!  | (2 bytes)  |                                        |
+//!  |____________|________________________________________|
+//!  |   func    |   func    |   func    |                 |
+//!  |  arg #1   |  arg #2   |  arg #3   |                 |
+//!  |   value   |   value   |   value   |     . . . .     |
+//!  | (i32/i64) | (i32/i64) | (i32/i64) |                 |
 //!  |___________|___________|___________|_________________|
 //!
 
