3f39f0ec29b516d0a123a4055c71c7f7759872a3
Yaron Wittenstein <yaron.wittenstein@gmail.com>
2020-02-20 13:59:23 +0200

    Refactoring rustdoc example for `Address & svm_byte_array`.

--- crates/svm-runtime-c-api/src/address.rs
+++ crates/svm-runtime-c-api/src/address.rs
@@ -9,11 +9,7 @@ use svm_common::Address;
 /// use svm_common::Address;
 /// use svm_runtime_c_api::svm_byte_array;
 ///
-/// let addr = Address:of("@someone");
-/// let bytes = svm_byte_array {
-///   bytes: addr.as_ptr(),
-///   length: Address::len() as u32,
-/// };
+/// let bytes: svm_byte_array = Address:of("@someone").into();
 ///
 /// let addr: Result<Address, String> = bytes.into();
 /// assert_eq!(Address::of("@someone"), addr.unwrap());
