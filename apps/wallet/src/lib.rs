d98027b9457e452626a6e36be81b72403ebbd49a
Yaron Wittenstein <yaron.wittenstein@gmail.com>
2020-02-24 15:06:46 +0200

    Adding app-storage variables offsets.

--- apps/wallet/src/lib.rs
+++ apps/wallet/src/lib.rs
@@ -1,45 +1,57 @@
 #![no_std]
 #![allow(unused)]
 
-//! The `SVM` wallet app is intended to serve as a 2-3 MultiSig Wallet
+//! The `SVM` wallet app is intended to serve as a MultiSig Wallet
 //! For managing Spacemesh investors coins.
 //!
 //! Link to the product specification:
 //! https://github.com/spacemeshos/product/blob/master/svm-wallet.md
 //!
+//!
+//!  App Storage Layout
+//!  ==================
+//!
+//!             Page #1
+//!  +-----------------------------+
+//!  |  pub_key1        (32 bytes) |
+//!  |-----------------------------+
+//!  |  pub_key2        (32 bytes) |
+//!  |-----------------------------+
+//!  |  pub_key3        (32 bytes) |
+//!  |-----------------------------+
+//!  |  daily_limit     (2 bytes)  |
+//!  +-----------------------------+
+//!  |  start_vesting   (8 bytes)  |
+//!  +-----------------------------+
+//!  |  vesting_months  (2 bytes)  |
+//!  +-----------------------------+
+//!  |  last_sync_layer (8 bytes)  |
+//!  +-----------------------------+
+//!  |  balance         (4 bytes)  |
+//!  +-----------------------------+
+//!  |  vested          (4 bytes)  |
+//!  +-----------------------------+
+//!
+//!  Offsets:
+//!  * pub_key1        - offset=0,   length=32
+//!  * pub_key2        - offset=32,  length=32
+//!  * pub_key3        - offset=64,  length=32
+//!  * daily_limit     - offset=96,  length=2
+//!  * start_vesting   - offset=98,  length=8
+//!  * vesting_months  - offset=106, length=2
+//!  * last_sync_layer - offset=108, length=8
+//!  * balance         - offset=116, length=4
+//!  * vested          - offset=120, length=4
+//!
+//! Total storage: 124 bytes.
+//!
 
-///
-/// The `init` function assumes the following `func_buf`
-/// +---------------------------------+
-/// |  pub_key1 | pub_key2 | pub_key3 |
-/// +---------------------------------+
-///
-/// The byte-length of each master-key is determined by function argument `key_size`.
-/// So that total length of func buf is `3 * key_size`.
-///
-///
-/// App Storage Layout
-/// ==================
-///
-///             Page #1
-/// +-----------------------------+
-/// |  pub_key1        (32 bytes) |
-/// |-----------------------------+
-/// |  pub_key2        (32 bytes) |
-/// |-----------------------------+
-/// |  pub_key3        (32 bytes) |
-/// |-----------------------------+
-/// |  daily_limit     (2 bytes)  |
-/// +-----------------------------+
-/// |  start_vesting   (8 bytes)  |
-/// +-----------------------------+
-/// |  vesting_months  (2 bytes)  |
-/// +-----------------------------+
-/// |  last_sync_layer (2 bytes)  |
-/// +-----------------------------+
-/// |  balance         (4 bytes)  |
-/// +-----------------------------+
 ///  
+/// The `init` function assumes the following `func_buf`
+/// +-----------------------------------------------------------------+
+/// | pub_key1 (32 bytes) | pub_key2 (32 bytes) | pub_key3 (32 bytes) |
+/// +-----------------------------------------------------------------+
+///
 #[no_mangle]
 pub extern "C" fn init(daily_limit: i64, start_vesting: i64, vesting_months: i32) {
     // 1) read `func-buf` into app-storage
@@ -60,8 +72,9 @@ pub extern "C" fn get_vested() -> i32 {
 pub extern "C" fn get_unvested() -> i32 {
     auth();
 
-    // 1) let vested = update_vesting();
+    // 1  update_vesting();
     // 2) calculate maximum vesting
+    // 3) vested <- read_vested()
     // 3) calculate `unvested = maximum_vesting - vested`
 
     todo!()
@@ -144,8 +157,14 @@ extern "C" fn update_vesting() -> i32 {
     // 5) calculate layer `layer_diff = layer_id - last_sync_layer`
     // 6) translate `layer_diff` to `diff_time`
     //    `diff_time_secs = layer_diff * layer_epoch_secs`
-    // 7)
+    // 7) `last_sync_layer <- layer_id`
+    // 8)
 
+    todo!()
+}
+
+#[no_mangle]
+extern "C" fn read_balance() -> i32 {
     todo!()
 }
 
