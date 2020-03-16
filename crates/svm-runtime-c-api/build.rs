bf85f01a92530f4b9819a04cf9008c8cf2b52251
Moshe Shababo <dicentralized@gmail.com>
2020-03-16 12:03:53 +0200

    PR fix

--- crates/svm-runtime-c-api/build.rs
+++ crates/svm-runtime-c-api/build.rs
@@ -10,13 +10,13 @@ fn generate_svm_header() {
     let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
     let out_dir = env::var("OUT_DIR").unwrap();
 
-    // targeting the workspace 'target/release' dir
+    // targeting the workspace 'target/(debug|release) dir
     let mut src_header = PathBuf::from(&out_dir);
     src_header.pop();
     src_header.pop();
     src_header.pop();
-    src_header.pop();
-    src_header.push("release/svm.h");
+    src_header.push("svm");
+    src_header.set_extension("h");
 
     // build using cbindgen
     Builder::new()
