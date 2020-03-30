b3f970890d579abc093e3fa42f7328f003298154
Yaron Wittenstein <yaron.wittenstein@gmail.com>
2020-03-30 20:34:19 +0300

    vmcall gas metering - takes into account the `gas_metering_enabled` flag.

--- crates/svm-runtime/src/gas/pricing.rs
+++ crates/svm-runtime/src/gas/pricing.rs
@@ -1,10 +1,15 @@
 /// Updates current running `App`'s `left gas`.
 #[macro_export]
 macro_rules! use_gas {
-    ($vmcall:expr, $ctx:expr) => {{
-        use crate::helpers;
+    ($vmcall:expr, $wasmer_ctx:expr) => {{
+        use crate::ctx::SvmCtx;
 
-        let gas = 10;
-        helpers::use_gas($ctx, gas);
+        let ctx: &mut SvmCtx = unsafe { svm_common::from_raw_mut::<SvmCtx>($wasmer_ctx.data) };
+
+        if ctx.gas_metering_enabled {
+            let gas = 10;
+
+            helpers::use_gas($wasmer_ctx, gas);
+        }
     }};
 }
