1910e118265b161c6ff79ae47622a651ac5df5bb
Yaron Wittenstein <yaron.wittenstein@gmail.com>
2020-04-01 12:51:53 +0300

    `MaybeGas` - adding tests.

--- crates/svm-runtime/src/gas/maybe_gas.rs
+++ crates/svm-runtime/src/gas/maybe_gas.rs
@@ -23,6 +23,11 @@ impl MaybeGas {
     }
 
     #[inline]
+    pub fn is_none(&self) -> bool {
+        self.0.is_none()
+    }
+
+    #[inline]
     pub fn unwrap(&self) -> u64 {
         self.0.unwrap()
     }
@@ -91,7 +96,7 @@ impl PartialOrd<u64> for MaybeGas {
 impl PartialOrd<MaybeGas> for u64 {
     #[inline]
     fn partial_cmp(&self, rhs: &MaybeGas) -> Option<Ordering> {
-        rhs.partial_cmp(self)
+        rhs.partial_cmp(self).map(|ord| ord.reverse())
     }
 }
 
@@ -123,9 +128,85 @@ impl From<u64> for MaybeGas {
 mod tests {
     use super::*;
 
+    macro_rules! assert_none {
+        ($expr:expr) => {{
+            assert!($expr.is_none());
+        }};
+    }
+
+    macro_rules! assert_gas {
+        ($maybe_gas:expr, $int:expr) => {{
+            assert_eq!($maybe_gas.unwrap(), $int);
+        }};
+    }
+
+    #[test]
+    fn maybe_gas_from() {
+        let gas = MaybeGas::from(10);
+        assert_eq!(gas, 10);
+    }
+
+    #[test]
+    fn maybe_gas_partial_eq() {
+        let gas1 = MaybeGas::from(10);
+        let gas2 = MaybeGas::from(10);
+        let gas3 = MaybeGas::from(30);
+
+        assert_eq!(gas1, 10);
+        assert_ne!(gas1, 20);
+
+        assert_eq!(10, gas1);
+        assert_ne!(20, gas1);
+
+        assert_eq!(gas1, gas1);
+        assert_eq!(gas1, gas2);
+
+        assert_ne!(gas1, gas3);
+    }
+
+    #[test]
+    fn maybe_gas_partial_ord() {
+        let gas = MaybeGas::from(10);
+
+        assert!(gas > 9);
+        assert!(gas >= 10);
+        assert!(!(gas > 10));
+
+        assert!(9 < gas);
+        assert!(10 <= gas);
+        assert!(!(10 < gas));
+    }
+
     #[test]
     fn maybe_gas_add() {
         let gas = MaybeGas::with(0);
-        todo!()
+        assert_gas!(gas, 0);
+
+        let gas = gas + 5;
+        assert_gas!(gas, 5);
+
+        let gas = gas + 10;
+        assert_gas!(gas, 15);
+    }
+
+    #[test]
+    fn maybe_gas_add_assign() {
+        let mut gas = MaybeGas::with(10);
+        assert_gas!(gas, 10);
+
+        gas += 20;
+        assert_gas!(gas, 30);
+    }
+
+    #[test]
+    fn maybe_gas_sub() {
+        let gas = MaybeGas::with(30);
+
+        let gas = gas - 10;
+        assert_gas!(gas.unwrap(), 20);
+
+        let gas = MaybeGas::with(15);
+        let gas = gas - 20;
+        assert_eq!(Err(OOGError {}), gas);
     }
 }
