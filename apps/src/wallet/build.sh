c25fecd676029c1afaee0afbbec128ff6b8ebcab
Moshe Shababo <dicentralized@gmail.com>
2020-04-16 15:12:40 +0300

    change desc

--- apps/src/wallet/build.sh
+++ apps/src/wallet/build.sh
@@ -1,7 +1,7 @@
 cargo +nightly build --target wasm32-unknown-unknown --release
 cp target/wasm32-unknown-unknown/release/*.wasm ../../../apps/wasm
 
-# compress the wasm artifact
+# optimize the wasm artifact in order to reduce size
 pushd ../../wasm
 wasm2wat wallet.wasm > wallet.wast
 rm wallet.wasm
