b747cbca3fa2dd35bc708a0395d67917f415cf1e
Moshe Shababo <dicentralized@gmail.com>
2020-04-16 14:53:58 +0300

    Fix multisig wallet WASM build

--- apps/src/wallet/build.sh
+++ apps/src/wallet/build.sh
@@ -1,5 +1,7 @@
 cargo +nightly build --target wasm32-unknown-unknown --release
 cp target/wasm32-unknown-unknown/release/*.wasm ../../../apps/wasm
+
+# compress the wasm artifact
 pushd ../../wasm
 wasm2wat wallet.wasm > wallet.wast
 rm wallet.wasm
