3ed5065388b18d35d8d939c1fadef5b798546eb0
Yaron Wittenstein <yaron.wittenstein@gmail.com>
2020-05-11 13:07:41 +0300

    WIP: stateful kv (mock).

--- crates/svm-storage2/src/kv/mock/stateful.rs
+++ crates/svm-storage2/src/kv/mock/stateful.rs
@@ -1,8 +1,9 @@
 use std::{cell::RefCell, collections::HashMap, rc::Rc};
 
-use svm_common::State;
+use svm_common::{DefaultKeyHasher, KeyHasher, State};
 use svm_kv::traits::KVStore;
 
+#[derive(Debug)]
 struct Node {
     state: State,
 
@@ -27,7 +28,7 @@ impl Node {
             Some(value) => Some(value.to_vec()),
             None => match &self.prev {
                 None => None,
-                Some(p) => p.get(key),
+                Some(prev) => prev.get(key),
             },
         }
     }
@@ -46,20 +47,19 @@ impl KVStore for StatefulKV {
     }
 
     fn store(&mut self, changes: &[(&[u8], &[u8])]) {
-        let data = changes
+        let changes: HashMap<_, _> = changes
             .iter()
             .map(|(k, v)| (k.to_vec(), v.to_vec()))
             .collect();
 
         let old_head = self.head.take();
-        let state = State::empty();
         let mut head = Node::empty();
 
-        head.data = data;
-        head.state = state.clone();
+        head.state = self.compute_state(&changes, &self.state);
+        head.data = changes;
         head.prev = old_head.map(|head| Box::new(head));
 
-        self.state = state;
+        self.state = head.state.clone();
         self.head = Some(head);
     }
 }
@@ -71,6 +71,53 @@ impl StatefulKV {
             head: None,
         }
     }
+
+    fn compute_state(&self, changes: &HashMap<Vec<u8>, Vec<u8>>, old_state: &State) -> State {
+        let capacity = changes
+            .iter()
+            .fold(State::len(), |acc, (k, v)| acc + k.len() + v.len());
+
+        let mut buf: Vec<u8> = Vec::with_capacity(capacity);
+
+        buf.extend_from_slice(old_state.as_slice());
+
+        for (k, v) in changes.iter() {
+            buf.extend_from_slice(k);
+            buf.extend_from_slice(v);
+        }
+
+        let bytes = DefaultKeyHasher::hash(&buf);
+        debug_assert_eq!(bytes.len(), State::len());
+
+        State::from(&bytes[..])
+    }
+
+    fn find_node<'a>(&'a self, state: &State) -> Option<&'a Box<Node>> {
+        fn _find_node<'a>(node: Option<&'a Node>, state: &State) -> Option<&'a Box<Node>> {
+            if node.is_none() {
+                return None;
+            }
+
+            let node: &Node = node.unwrap();
+            let boxed_prev = node.prev.as_ref();
+
+            if node.state.as_slice() == state.as_slice() {
+                return boxed_prev;
+            }
+
+            let prev: Option<&Node> = boxed_prev.map(|p| &**p);
+
+            _find_node(prev, state)
+        }
+
+        let head = self.head.as_ref();
+
+        if self.head.is_none() {
+            return None;
+        }
+
+        _find_node(head, state)
+    }
 }
 
 #[cfg(test)]
@@ -81,5 +128,82 @@ mod tests {
     fn mock_kv_empty() {
         let kv = StatefulKV::new();
         assert_eq!(kv.state, State::empty());
+    }
+
+    #[test]
+    fn mock_kv_single_state() {
+        let mut kv = StatefulKV::new();
+
+        let (k1, v1) = (b"aaa", vec![0x10, 0x20]);
+        let (k2, v2) = (b"bbb", vec![0x30, 0x40, 0x50]);
+
+        let changes = vec![(&k1[..], &v1[..]), (&k2[..], &v2[..])];
+        kv.store(&changes[..]);
+
+        assert_ne!(kv.state, State::empty());
+
+        assert_eq!(kv.get(&k1[..]).unwrap(), v1);
+        assert_eq!(kv.get(&k2[..]).unwrap(), v2);
+    }
+
+    #[test]
+    fn mock_kv_two_states() {
+        let mut kv = StatefulKV::new();
+
+        let (k1, v1) = (b"aaa", vec![0x10, 0x20]);
+        let (k2, v2) = (b"bbb", vec![0x30, 0x40, 0x50]);
+
+        let changes = vec![(&k1[..], &v1[..]), (&k2[..], &v2[..])];
+        kv.store(&changes[..]);
+
+        let (k3, v3) = (b"ccc", vec![0x60, 0x70]);
+        let (k4, v4) = (b"ddd", vec![0x80, 0x90]);
+
+        let changes = vec![(&k3[..], &v3[..]), (&k4[..], &v4[..])];
+        kv.store(&changes[..]);
+
+        assert_eq!(kv.get(&k1[..]).unwrap(), v1);
+        assert_eq!(kv.get(&k2[..]).unwrap(), v2);
+        assert_eq!(kv.get(&k3[..]).unwrap(), v3);
+        assert_eq!(kv.get(&k4[..]).unwrap(), v4);
+    }
+
+    #[test]
+    fn mock_kv_three_states() {
+        let mut kv = StatefulKV::new();
+
+        let (k1, v1) = (b"aaa", vec![0x10, 0x20]);
+        let changes = vec![(&k1[..], &v1[..])];
+        kv.store(&changes[..]);
+
+        let (k2, v2) = (b"bbb", vec![0x30, 0x40, 0x50]);
+        let changes = vec![(&k2[..], &v2[..])];
+        kv.store(&changes[..]);
+
+        let (k3, v3) = (b"ccc", vec![0x60, 0x70]);
+        let changes = vec![(&k3[..], &v3[..])];
+        kv.store(&changes[..]);
+
+        assert_eq!(kv.get(&k1[..]).unwrap(), v1);
+        assert_eq!(kv.get(&k2[..]).unwrap(), v2);
+        assert_eq!(kv.get(&k3[..]).unwrap(), v3);
+    }
+
+    #[test]
+    fn mock_kv_update_a_key_value() {
+        let mut kv = StatefulKV::new();
+
+        let (k, v1) = (b"aaa", vec![0x10, 0x20]);
+
+        let changes = vec![(&k[..], &v1[..])];
+        kv.store(&changes[..]);
+
+        assert_eq!(kv.get(&k[..]).unwrap(), v1);
+
+        let (k, v2) = (b"aaa", vec![0x30, 0x40]);
+        let changes = vec![(&k[..], &v2[..])];
+        kv.store(&changes[..]);
+
+        assert_eq!(kv.get(&k[..]).unwrap(), v2);
     }
 }
