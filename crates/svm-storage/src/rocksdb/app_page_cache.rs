afdaabf6106a19a55bfd1fabb19b1fda6b96b5e0
Yaron Wittenstein <yaron.wittenstein@gmail.com>
2020-05-19 14:23:22 +0300

    delete `svm-storage` crate.

--- crates/svm-storage/src/rocksdb/app_page_cache.rs
+++
@@ -1,5 +0,0 @@
-use crate::default::DefaultPageCache;
-use crate::rocksdb::RocksdbAppPages;
-
-/// `DefaultPageCache` implementation backed by `RocksdbAppPages` pages-storage.
-pub type RocksdbAppPageCache = DefaultPageCache<RocksdbAppPages>;
--- crates/svm-storage/src/testing/mod.rs
+++
@@ -1,9 +0,0 @@
-mod app_page_cache;
-mod app_pages;
-mod app_storage;
-mod page;
-
-pub use app_page_cache::*;
-pub use app_pages::*;
-pub use app_storage::*;
-pub use page::*;
