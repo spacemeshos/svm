5d7fef2b7de9683d20c3a646789df7ddb9111913
Yaron Wittenstein <yaron.wittenstein@gmail.com>
2020-06-17 19:39:44 +0300

    WASM buffer `alloc / free` works.

--- crates/svm-codec/src/api/wasm/mod.rs
+++ crates/svm-codec/src/api/wasm/mod.rs
@@ -12,57 +12,70 @@ const HEADER_SIZE: usize = 8;
 ///
 /// The reason we need both `length` and `capacity` and due to the implementation
 /// of Rust `Vec`. Even though we use `Vec::with_capacity` we still prefer to store
-/// explicitly in the `Header` the `capacity` returned by `Vec#into_raw_parts`
+/// explicitly in the `Header` the `capacity` returned by `Vec#into_raw_parts`.
+///
+/// See also `Vec#reserve_exact` documentation:
+///
+/// ```md
+/// Note that the allocator may give the collection more space than it
+/// requests. Therefore, capacity can not be relied upon to be precisely
+/// minimal. Prefer `reserve` if future insertions are expected.
+/// ```
 ///
 /// ### Buffer Header Layout
 ///
 /// +------------------+--------------------+
-/// | Length (4 bytes) | Capacity (4 bytes) |
+/// | length (4 bytes) | capacity (4 bytes) |
 /// +------------------+--------------------+
 ///
-/// Both `Length` and `Capacity` are laid out in Big-Endian order
+/// Both `length` and `capacity` are laid out in Big-Endian order
 ///
-#[no_mangle]
-pub unsafe extern "C" fn alloc(length: i32) -> i32 {
-    let buf_len = HEADER_SIZE + length as usize;
+pub fn alloc(length: usize) -> usize {
+    let buf_len = HEADER_SIZE + length;
     let mut buf = vec![0; buf_len];
 
     let (ptr, len, cap) = buf.into_raw_parts();
+    debug_assert_eq!(len, buf_len);
+
+    // We subtract the `HEADER_SIZE` from `len` and `cap`.
+    // Now they will refer to the actual buffer data size.
+    // The method `free` should take that into account.
+    let len = len - HEADER_SIZE;
+    let cap = cap - HEADER_SIZE;
 
     write_header_u32(ptr, len as u32, 0);
     write_header_u32(ptr, cap as u32, 4);
 
-    ptr as *const u8 as i32
+    ptr as usize
 }
 
-#[no_mangle]
-pub unsafe extern "C" fn free(ptr: i32) {
+pub fn free(ptr: usize) {
     let ptr = ptr as *mut u8;
 
     let len = read_header_u32(ptr, 0);
     let cap = read_header_u32(ptr, 4);
 
-    let _vec = Vec::from_raw_parts(ptr, len as usize, cap as usize);
+    let len = len as usize + HEADER_SIZE;
+    let cap = cap as usize + HEADER_SIZE;
+
+    let _vec = unsafe { Vec::from_raw_parts(ptr, len, cap) };
 }
 
 fn write_header_u32(buf: *mut u8, n: u32, off: usize) {
-    let ptr = unsafe { buf.offset(off as isize) };
-    let slice = unsafe { std::slice::from_raw_parts_mut(ptr, 4) };
+    unsafe {
+        let ptr = buf.add(off);
+        let slice = std::slice::from_raw_parts_mut(ptr, 4);
 
-    BigEndian::write_u32(slice, n)
+        BigEndian::write_u32(slice, n)
+    }
 }
 
 fn read_header_u32(buf: *mut u8, off: usize) -> u32 {
-    let slice = header_slice(buf, off);
-
-    BigEndian::read_u32(slice)
-}
-
-#[inline]
-fn header_slice<'a>(buf: *mut u8, off: usize) -> &'a mut [u8] {
     unsafe {
-        let ptr = buf.offset(off as isize);
-        std::slice::from_raw_parts_mut(ptr, 4)
+        let ptr = buf.add(off);
+        let slice = std::slice::from_raw_parts(ptr, 4);
+
+        BigEndian::read_u32(slice)
     }
 }
 
@@ -72,9 +85,29 @@ mod test {
 
     #[test]
     fn buffer_alloc_and_free() {
-        let buf: &'static [u8] = b"Hello World";
-        // let len = buf.len() as i32;
+        let data: &'static [u8] = b"Hello World";
+        let len = data.len();
 
-        // let ptr = unsafe { alloc(len) };
+        let buf_ptr = alloc(len) as *mut u8;
+        let buf_len = HEADER_SIZE + len;
+
+        let buf: &mut [u8] = unsafe { std::slice::from_raw_parts_mut(buf_ptr, buf_len) };
+
+        let src = data.as_ptr();
+        let dst = buf.as_mut_ptr();
+
+        unsafe {
+            std::ptr::copy(src, dst.add(HEADER_SIZE), len as usize);
+        }
+
+        // assert buffer Header `length` and `capacity` fields
+        assert_eq!(BigEndian::read_u32(&buf[0..4]), len as u32);
+        assert_eq!(BigEndian::read_u32(&buf[4..8]), len as u32);
+
+        // assert the buffer data
+        assert_eq!(&buf[HEADER_SIZE..(HEADER_SIZE + len)], b"Hello World");
+
+        // freeing the buffer
+        free(buf_ptr as usize);
     }
 }
