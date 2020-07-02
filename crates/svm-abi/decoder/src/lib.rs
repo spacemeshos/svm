f7441737024b0defe052fb318c8729b8b3c20ccd
Yaron Wittenstein <yaron.wittenstein@gmail.com>
2020-07-02 16:09:55 +0300

    `svm-decoder` (with `no_std`) and `svm-codec` compile to `.wasm`

--- crates/svm-abi/decoder/src/lib.rs
+++ crates/svm-abi/decoder/src/lib.rs
@@ -1,7 +1,5 @@
 #![no_std]
 #![feature(lang_items)]
-#![cfg_attr(feature = "alloc", feature(alloc))]
-#![cfg_attr(not(feature = "std"), no_std)]
 #![allow(missing_docs)]
 #![allow(unused)]
 #![allow(dead_code)]
@@ -21,11 +19,6 @@ fn panic(_info: &PanicInfo) -> ! {
 #[cfg(not(test))]
 extern "C" fn rust_eh_personality() {}
 
-#[cfg(test)]
-#[macro_use]
-extern crate std;
-
-#[no_std]
 #[cfg(test)]
 mod test {
     use crate::decoder::{DecodeError, Decoder};
--- crates/svm-abi/encoder/src/lib.rs
+++ crates/svm-abi/encoder/src/lib.rs
@@ -1,4 +1,3 @@
-#![cfg_attr(not(feature = "std"), no_std)]
 #![allow(missing_docs)]
 #![allow(unused)]
 #![allow(dead_code)]
