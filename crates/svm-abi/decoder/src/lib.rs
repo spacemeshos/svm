735c0ede08d81007783915c6f0ae532bb0e4e366
Yaron Wittenstein <yaron.wittenstein@gmail.com>
2020-07-02 16:05:48 +0300

    `svm-abi-decoder` compiles without `std` to `.wasm`

--- crates/svm-abi/decoder/src/lib.rs
+++ crates/svm-abi/decoder/src/lib.rs
@@ -1,13 +1,31 @@
 #![no_std]
+#![feature(lang_items)]
+#![cfg_attr(feature = "alloc", feature(alloc))]
 #![cfg_attr(not(feature = "std"), no_std)]
 #![allow(missing_docs)]
 #![allow(unused)]
 #![allow(dead_code)]
 #![allow(unreachable_code)]
 
-#[cfg(not(feature = "std"))]
 pub mod decoder;
 
+use core::panic::PanicInfo;
+
+#[panic_handler]
+#[cfg(not(test))]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+#[lang = "eh_personality"]
+#[cfg(not(test))]
+extern "C" fn rust_eh_personality() {}
+
+#[cfg(test)]
+#[macro_use]
+extern crate std;
+
+#[no_std]
 #[cfg(test)]
 mod test {
     use crate::decoder::{DecodeError, Decoder};
