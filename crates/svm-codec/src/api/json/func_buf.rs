296eead4b026c1c6a69d8916f4ec73fcfdffa8e7
Yaron Wittenstein <yaron.wittenstein@gmail.com>
2020-07-08 21:13:33 +0300

    api/json/func_buf.rs - happy-path tests pass.

--- crates/svm-codec/src/api/json/func_buf.rs
+++ crates/svm-codec/src/api/json/func_buf.rs
@@ -4,7 +4,9 @@ use crate::{
 };
 
 use svm_abi_encoder::Encoder;
-use svm_sdk::value::{AddressOwned, Array, Primitive, PubKey256Owned, Value};
+use svm_sdk::value::{
+    Address, AddressOwned, Array, Composite, Primitive, PubKey256, PubKey256Owned, Value,
+};
 use svm_sdk::{self as sdk};
 
 ///
@@ -31,10 +33,8 @@ pub fn encode_func_buf(json: &json::Value) -> Result<Vec<u8>, JsonError> {
 
     let mut buf = Vec::new();
 
-    for (i, ty) in abi.iter().enumerate() {
-        let value = &data[i];
-
-        encode_value(ty, value, i, &mut buf)?;
+    for (i, (ty, raw)) in abi.iter().zip(data).enumerate() {
+        encode_value(ty, raw, i, &mut buf)?;
     }
 
     Ok(buf)
@@ -42,13 +42,13 @@ pub fn encode_func_buf(json: &json::Value) -> Result<Vec<u8>, JsonError> {
 
 fn encode_value(
     ty: &json::Value,
-    value: &json::Value,
+    raw: &json::Value,
     pos: usize,
     buf: &mut Vec<u8>,
 ) -> Result<(), JsonError> {
     match ty {
-        json::Value::String(..) => encode_primitive(ty, value, pos, buf),
-        json::Value::Array(..) => encode_array(ty, value, pos, buf),
+        json::Value::String(..) => encode_primitive(ty, raw, pos, buf),
+        json::Value::Array(..) => encode_array(ty, raw, pos, buf),
         _ => Err(JsonError::InvalidField {
             field: "abi".to_string(),
             reason: "`abi` expects only `string` or `Array` items".to_string(),
@@ -56,92 +56,9 @@ fn encode_value(
     }
 }
 
-macro_rules! do_encode_array {
-    ($values:expr, $ty:ty, $func:expr, $pos:expr, $buf:expr) => {{
-        let mut vec = Vec::new();
-
-        for (i, json) in $values.iter().enumerate() {
-            let field = format!("data[{}], item={}", $pos, i);
-            let value = json::as_string(json, &field)?;
-
-            let prim = $func(&value, &field)?;
-            vec.push(prim);
-
-            vec.encode($buf);
-        }
-    }};
-}
-
-macro_rules! do_encode_primitive {
-    ("address", $json:expr, $field:expr, $buf:expr) => {{
-        let addr: AddressOwned = str_as_addr(value, field)?;
-        addr.encode(buf);
-        Ok(())
-    }};
-
-    ("pubkey256", $json:expr, $field:expr, $buf:expr) => {{
-        let pkey: PubKey256 = str_as_pubkey256(value, field)?;
-        pkey.encode(buf);
-        Ok(())
-    }};
-
-    ($ty:expr, $json:expr, $field:expr, $buf:expr) => {{
-        Err(JsonError::InvalidField {
-            field: "abi".to_string(),
-            reason: format!("invalid ABI type {}", $ty),
-        })
-    }};
-}
-
-fn encode_array(
-    ty: &json::Value,
-    value: &json::Value,
-    pos: usize,
-    buf: &mut Vec<u8>,
-) -> Result<(), JsonError> {
-    let field = format!("abi[{}]", pos);
-    let ty = json::as_array(ty, &field)?;
-
-    if ty.len() != 1 {
-        return Err(JsonError::InvalidField {
-            field: "abi".to_string(),
-            reason: "`Array` items must be of length = 1".to_string(),
-        });
-    }
-
-    let ty = json::as_string(&ty[0], &format!("abi[{}][0]", pos))?;
-    let values = json::as_array(value, &format!("data[{}][0]", pos))?;
-
-    match ty.as_str() {
-        "address" => do_encode_array!(values, AddressOwned, str_as_addr, pos, buf),
-        "pubkey256" => do_encode_array!(values, PubKey256Owned, str_as_pubkey256, pos, buf),
-        _ => {
-            return Err(JsonError::InvalidField {
-                field: format!("data[{}]", pos),
-                reason: format!("unsupported `Array` of type: {}", ty),
-            });
-        }
-    }
-
-    Ok(())
-}
-
-fn encode_primitive(
-    ty: &json::Value,
-    value: &json::Value,
-    pos: usize,
-    buf: &mut Vec<u8>,
-) -> Result<(), JsonError> {
-    let ty = json::as_string(ty, &format!("abi[{}]", pos))?;
-    let value = json::as_string(value, &format!("data[{}]", pos))?;
-    let field = format!("data[{}]", pos);
-
-    do_encode_primitive!(ty, value, field, buf)
-}
-
 macro_rules! str_as_primitive {
-    ($s:expr, $ty:ty, $field:expr) => {{
-        let bytes = json::str_to_bytes($s, $field)?;
+    ($raw:expr, $ty:ty, $field:expr) => {{
+        let bytes = json::str_to_bytes($raw, $field)?;
 
         if bytes.len() != <$ty>::size() {
             return Err(JsonError::InvalidField {
@@ -155,16 +72,114 @@ macro_rules! str_as_primitive {
     }};
 }
 
-fn str_as_addr(s: &str, field: &str) -> Result<AddressOwned, JsonError> {
-    str_as_primitive!(s, AddressOwned, field)
+macro_rules! do_encode_primitive {
+    ("address", $raw:expr, $field:expr, $buf:expr) => {{
+        let prim = str_as_primitive!(&$raw, AddressOwned, $field)?;
+        prim.encode($buf);
+
+        Ok(())
+    }};
+
+    ("pubkey256", $raw:expr, $field:expr, $buf:expr) => {{
+        let pkey = str_as_primitive!($raw, PubKey256Owned, $field)?;
+        pkey.encode($buf);
+
+        Ok(())
+    }};
+
+    ($ty:expr, $field:expr, $buf:expr) => {{
+        return Err(JsonError::InvalidField {
+            field: "abi".to_string(),
+            reason: format!("invalid ABI type `{}`", $ty),
+        });
+    }};
 }
 
-fn str_as_pubkey256(s: &str, field: &str) -> Result<PubKey256Owned, JsonError> {
-    str_as_primitive!(s, PubKey256Owned, field)
+macro_rules! do_encode_array {
+    ($raw_array:expr, $ty:ty, $pos:expr, $buf:expr) => {{
+        let mut vec = Vec::new();
+
+        for (i, raw) in $raw_array.iter().enumerate() {
+            let field = format!("data[{}], item={}", $pos, i);
+            let raw = as_str(raw, &field)?;
+
+            let prim = str_as_primitive!(&raw, $ty, &field)?;
+            vec.push(prim);
+        }
+
+        vec.encode($buf);
+
+        Ok(())
+    }};
 }
 
-pub fn decode_func_buf(json: &json::Value) -> Result<Vec<u8>, JsonError> {
-    Ok(Vec::new())
+fn encode_array(
+    ty: &json::Value,
+    raw: &json::Value,
+    pos: usize,
+    buf: &mut Vec<u8>,
+) -> Result<(), JsonError> {
+    let ty = as_array(ty, &format!("abi[{}]", pos))?;
+
+    if ty.len() != 1 {
+        return Err(JsonError::InvalidField {
+            field: "abi".to_string(),
+            reason: "`Array` items must be of length = 1`".to_string(),
+        });
+    }
+
+    let ty = as_str(&ty[0], &format!("abi[{}][0]", pos))?;
+    let raw = as_array(raw, &format!("data[{}][0]", pos))?;
+
+    match ty {
+        "address" => do_encode_array!(raw, AddressOwned, pos, buf),
+        "pubkey256" => do_encode_array!(raw, PubKey256Owned, pos, buf),
+        _ => Err(JsonError::InvalidField {
+            field: format!("data[{}]", pos),
+            reason: format!("unsupported `Array` of type: {}", ty),
+        }),
+    }
+}
+
+fn encode_primitive(
+    ty: &json::Value,
+    raw: &json::Value,
+    pos: usize,
+    buf: &mut Vec<u8>,
+) -> Result<(), JsonError> {
+    let ty = as_str(ty, &format!("abi[{}]", pos))?;
+    let raw = as_str(raw, &format!("data[{}]", pos))?;
+    let field = format!("data[{}]", pos);
+
+    match ty {
+        "address" => do_encode_primitive!("address", raw, &field, buf),
+        "pubkey256" => do_encode_primitive!("pubkey256", raw, &field, buf),
+        _ => do_encode_primitive!(ty, &field, buf),
+    }
+}
+
+pub fn decode_func_buf(_raw: &json::Value) -> Result<Vec<u8>, JsonError> {
+    todo!()
+}
+
+fn as_str<'a>(raw: &'a json::Value, field: &str) -> Result<&'a str, JsonError> {
+    match raw.as_str() {
+        None => Err(JsonError::InvalidField {
+            field: field.to_string(),
+            reason: format!("value `{}` isn't a string", raw),
+        }),
+        Some(raw) => Ok(raw),
+    }
+}
+
+fn as_array<'a>(raw: &'a json::Value, field: &str) -> Result<&'a Vec<json::Value>, JsonError> {
+    match raw.as_array() {
+        None => Err(JsonError::InvalidField {
+            field: field.to_string(),
+            reason: format!("value `{}` isn't an Array", raw),
+        }),
+        Some(raw) => Ok(raw),
+    }
 }
 
 #[cfg(test)]
@@ -181,7 +196,9 @@ mod tests {
             let m = $n / $bytes.len();
             let vec = (&$bytes[..]).repeat(m);
 
-            unsafe { core::mem::transmute::<*const u8, &[u8; $n]>(&vec[0]) }
+            let vec = Box::leak(Box::new(vec));
+
+            unsafe { core::mem::transmute::<*const u8, &[u8; $n]>(vec.as_ptr()) }
         }};
     }
 
@@ -284,26 +301,53 @@ mod tests {
     pub fn json_encode_func_buf_address_array() {
         let json = json!({
             "abi": [["address"]],
-            "data": [[addr!("1020304050"), addr!("60708090A)")]]
+            "data": [[addr!("1020304050"), addr!("60708090A0")]]
         });
 
-        let bytes = encode_func_buf(&json);
-        dbg!(bytes);
-        // let decoder = Decoder::new();
-        // let mut cursor = Cursor::new(&bytes);
-        // let actual = decoder.decode_value(&mut cursor).unwrap();
+        let bytes = encode_func_buf(&json).unwrap();
+        let decoder = Decoder::new();
+        let mut cursor = Cursor::new(&bytes);
+        let actual = decoder.decode_value(&mut cursor).unwrap();
 
-        // let expected = Value::Primitive(Primitive::Address(Address(extend!(
-        //     vec![0x10, 0x20, 0x30, 0x40, 0x50],
-        //     20
-        // ))));
+        let addr1 = Value::Primitive(Primitive::Address(Address(extend!(
+            vec![0x10, 0x20, 0x30, 0x40, 0x50],
+            20
+        ))));
 
-        // assert_eq!(expected, actual);
+        let addr2 = Value::Primitive(Primitive::Address(Address(extend!(
+            vec![0x60, 0x70, 0x80, 0x90, 0xA0],
+            20
+        ))));
+
+        let addrs = [addr1, addr2];
+        let expected = Value::Composite(Composite::Array(&addrs));
+        assert_eq!(expected, actual);
     }
 
-    #[ignore]
     #[test]
     pub fn json_encode_func_buf_pubkey256_array() {
-        todo!()
+        let json = json!({
+            "abi": [["pubkey256"]],
+            "data": [[pkey!("10203040"), pkey!("A0B0C0D0")]]
+        });
+
+        let bytes = encode_func_buf(&json).unwrap();
+        let decoder = Decoder::new();
+        let mut cursor = Cursor::new(&bytes);
+        let actual = decoder.decode_value(&mut cursor).unwrap();
+
+        let pkey1 = Value::Primitive(Primitive::PubKey256(PubKey256(extend!(
+            vec![0x10, 0x20, 0x30, 0x40],
+            32
+        ))));
+
+        let pkey2 = Value::Primitive(Primitive::PubKey256(PubKey256(extend!(
+            vec![0xA0, 0xB0, 0xC0, 0xD0],
+            32
+        ))));
+
+        let pkeys = [pkey1, pkey2];
+        let expected = Value::Composite(Composite::Array(&pkeys));
+        assert_eq!(expected, actual);
     }
 }
