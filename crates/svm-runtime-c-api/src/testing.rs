66efce2a4821e171b321002d075a817c8334faf0
Yaron Wittenstein <yaron.wittenstein@gmail.com>
2020-08-30 14:33:35 +0300

    remove yarn.lock

--- crates/svm-runtime-c-api/src/testing.rs
+++ crates/svm-runtime-c-api/src/testing.rs
@@ -2,24 +2,23 @@ use std::ffi::c_void;
 
 use crate::svm_byte_array;
 
-use svm_runtime::ctx::SvmCtx;
+use svm_runtime::Context;
 
-use wasmer_runtime_c_api::instance::wasmer_instance_context_t;
-use wasmer_runtime_core::vm::Ctx;
+use wasmer_c_api::instance::wasmer_instance_context_t;
 
 /// Given a raw pointer to `wasmer` instance context, mutably borrows inner `data`
 /// and extract from it a pointer to the so called `host`.
 /// (it's type is defined as `T` in thefunction declaration)
 pub unsafe fn svm_host_get<'a, T>(raw_ctx: *mut wasmer_instance_context_t) -> &'a mut T {
-    let ctx = cast_to_wasmer_ctx(raw_ctx);
+    let ctx = cast_to_context(raw_ctx);
     let svm_ctx = svm_common::from_raw_mut::<SvmCtx>(ctx.data);
 
     &mut *(svm_ctx.host as *mut T)
 }
 
-/// Casts a raw pointer to wasmer instance context to it's Safe Rust version (`&mut Ctx`)
-pub unsafe fn cast_to_wasmer_ctx<'a>(ctx: *mut wasmer_instance_context_t) -> &'a mut Ctx {
-    &mut *(ctx as *mut Ctx)
+/// Casts a raw pointer to wasmer instance context to it's Safe Rust version (`&mut Context`)
+pub unsafe fn cast_to_context<'a>(ctx: *mut wasmer_instance_context_t) -> &'a mut Context {
+    &mut *(ctx as *mut Context)
 }
 
 /// Allocates `count` imports array, returns a pointer to the first import.
