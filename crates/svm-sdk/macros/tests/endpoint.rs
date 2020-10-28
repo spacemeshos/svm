6257267c3f18fe662bb9d630521526f9aa9759f2
Yaron Wittenstein <yaron.wittenstein@gmail.com>
2020-10-28 14:05:39 +0200

    Simplifying the `endpoint` sanity test.

--- crates/svm-sdk/macros/tests/endpoint.rs
+++ crates/svm-sdk/macros/tests/endpoint.rs
@@ -4,33 +4,45 @@ use cfg_if::cfg_if;
 
 cfg_if! {
     if #[cfg(not(windows))] {
-
         use svm_sdk::Amount;
         use svm_sdk::host::{MockHost, traits::Host};
+        use svm_abi_encoder::Encoder;
+        use svm_abi_decoder::CallData;
         use svm_sdk_macros::endpoint;
+
+        fn set_calldata<T: Encoder>(calldata: T)  {
+            let host = MockHost::instance();
+
+            host.set_calldata(calldata);
+        }
+
+        fn get_returndata() -> CallData {
+            let host = MockHost::instance();
+            let bytes = host.get_returndata();
+
+            let bytes = bytes.unwrap().leak();
+            let mut returndata = CallData::new(bytes);
+
+            returndata
+        }
 
         #[endpoint]
         fn add(a: i32, b: i32) -> (Amount, bool) {
             let c = a + b;
 
-            return (Amount(c as u64), false)
+            let amount = Amount(c as u64);
+            let err = false;
+
+            return (amount, err)
         }
 
         #[test]
         fn test_endpoint() {
-            use svm_abi_decoder::CallData;
-
-            let host = MockHost::instance();
-
-            let calldata = (10i32, 20i32);
-            host.set_calldata(calldata);
+            set_calldata((10i32, 20i32));
 
             add();
 
-            let bytes = host.get_returndata();
-
-            let bytes: &'static [u8] = bytes.unwrap().leak();
-            let mut returndata = CallData::new(bytes);
+            let mut returndata = get_returndata();
 
             let (amount, err): (Amount, bool) = returndata.next_2();
 
