4b435eded2ab7c7ba02a4b2765b47b38598b873f
Yaron Wittenstein <yaron.wittenstein@gmail.com>
2020-11-08 15:23:35 +0200

    Parsing `syn::Attribute` into `FuncAttribute`

--- crates/svm-sdk/macros/src/attr.rs
+++ crates/svm-sdk/macros/src/attr.rs
@@ -1,4 +1,10 @@
-use syn::{AttrStyle, Attribute, Result};
+use proc_macro2::token_stream::IntoIter;
+use proc_macro2::{Delimiter, Group, TokenStream, TokenTree};
+
+use quote::{quote, ToTokens};
+
+use syn::parse::{Parse, ParseStream};
+use syn::{braced, token, Attribute, Ident, Result, Token};
 
 #[derive(Debug)]
 pub enum FuncAttribute {
@@ -7,4 +13,138 @@ pub enum FuncAttribute {
     BeforeFund,
 
     Fundable(String),
+
+    Other(TokenStream),
+}
+
+#[derive(Debug)]
+pub enum FuncAttrKind {
+    Endpoint,
+
+    BeforeFund,
+
+    Fundable,
+
+    Other,
+}
+
+pub fn parse_attr(attr: &Attribute) -> Result<FuncAttribute> {
+    let kind = parse_attr_kind(attr)?;
+
+    let attr = match kind {
+        FuncAttrKind::Endpoint => {
+            assert!(attr.tokens.is_empty());
+
+            FuncAttribute::Endpoint
+        }
+        FuncAttrKind::BeforeFund => {
+            assert!(attr.tokens.is_empty());
+
+            FuncAttribute::BeforeFund
+        }
+        FuncAttrKind::Fundable => {
+            let tokens = attr.tokens.clone();
+            let mut iter = tokens.into_iter();
+
+            if let Some(TokenTree::Group(group)) = iter.next() {
+                assert_eq!(group.delimiter(), Delimiter::Parenthesis);
+
+                let stream = group.stream();
+                let ident = syn::parse2::<Ident>(stream)?;
+
+                FuncAttribute::Fundable(ident.to_string())
+            } else {
+                todo!("explain we expected a different input format")
+            }
+        }
+        FuncAttrKind::Other => FuncAttribute::Other(quote! { #attr }),
+    };
+
+    Ok(attr)
+}
+
+fn parse_attr_kind(attr: &Attribute) -> Result<FuncAttrKind> {
+    let mut tokens = TokenStream::new();
+
+    let path = &attr.path;
+    path.to_tokens(&mut tokens);
+
+    syn::parse2::<FuncAttrKind>(tokens)
+}
+
+impl Parse for FuncAttrKind {
+    fn parse(input: ParseStream) -> Result<Self> {
+        let ident: Ident = input.parse()?;
+        let ident_str = format!("{}", ident);
+        let ident_str = ident_str.as_str();
+
+        let kind = match ident_str {
+            "endpoint" => FuncAttrKind::Endpoint,
+            "fundable" => FuncAttrKind::Fundable,
+            "before_fund" => FuncAttrKind::BeforeFund,
+            _ => FuncAttrKind::Other,
+        };
+
+        Ok(kind)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    use proc_macro2::TokenStream;
+
+    use quote::quote;
+    use quote::ToTokens;
+
+    use syn::{parse_quote, Attribute};
+
+    #[test]
+    fn func_attr_endpoint() {
+        let attr: Attribute = parse_quote! {
+            #[endpoint]
+        };
+
+        let func_attr = parse_attr(&attr).unwrap();
+        assert!(matches!(func_attr, FuncAttribute::Endpoint));
+    }
+
+    #[test]
+    fn func_attr_before_fund() {
+        let attr: Attribute = parse_quote! {
+            #[before_fund]
+        };
+
+        let func_attr = parse_attr(&attr).unwrap();
+        assert!(matches!(func_attr, FuncAttribute::BeforeFund));
+    }
+
+    #[test]
+    fn func_attr_fundable() {
+        let attr: Attribute = parse_quote! {
+            #[fundable(deny_funding)]
+        };
+
+        let actual = parse_attr(&attr);
+        let expected = FuncAttribute::Fundable("deny_funding".to_string());
+
+        assert!(matches!(actual, expected));
+    }
+
+    #[test]
+    fn func_attr_other() {
+        let attr: Attribute = parse_quote! {
+            #[derive(Debug, Copy, Clone)]
+        };
+
+        let attr = parse_attr(&attr).unwrap();
+        assert!(matches!(attr, FuncAttribute::Other(..)));
+
+        if let FuncAttribute::Other(tokens) = attr {
+            assert_eq!(tokens.to_string(), "# [derive (Debug , Copy , Clone)]");
+        } else {
+            unreachable!()
+        }
+    }
 }
--- crates/svm-sdk/macros/src/function.rs
+++ crates/svm-sdk/macros/src/function.rs
@@ -1,12 +1,10 @@
-extern crate proc_macro;
-
 use proc_macro2::token_stream::IntoIter;
 use proc_macro2::{Delimiter, Group, Ident, Literal, Punct, Spacing, Span, TokenStream, TokenTree};
 
 use quote::{quote, ToTokens};
 
-use syn::parse::ParseStream;
-use syn::{Attribute, ItemFn};
+use syn::parse::{Parse, ParseStream};
+use syn::{Attribute, ItemFn, Result, Signature};
 
 use crate::FuncAttribute;
 
@@ -18,30 +16,80 @@ pub struct Param {
     //
 }
 
+pub struct Return {
+    //
+}
+
 impl Function {
     pub fn new(raw: ItemFn) -> Self {
         Self { raw }
     }
 
+    pub fn attrs(&self) -> &[FuncAttribute] {
+        todo!()
+    }
+
     pub fn name(&self) -> Ident {
-        self.raw.sig.ident.clone()
+        self.raw_sig().ident.clone()
     }
 
     pub fn params(&self) -> &[Param] {
         todo!()
     }
 
-    pub fn attrs(&self) -> &[FuncAttribute] {
+    pub fn returns(&self) -> &[Return] {
         todo!()
+    }
+
+    pub fn raw_sig(&self) -> &Signature {
+        &self.raw.sig
+    }
+
+    pub fn raw_sig_mut(&mut self) -> &mut Signature {
+        &mut self.raw.sig
+    }
+
+    pub fn take_raw_attrs(&mut self) -> Vec<Attribute> {
+        std::mem::replace(&mut self.raw.attrs, Vec::new())
     }
 }
 
-pub enum FuncAttrs {
-    Endpoint,
+impl ToTokens for Function {
+    fn to_tokens(&self, tokens: &mut TokenStream) {
+        let ast = quote! {
+            //
+        };
 
-    BeforeFund,
+        tokens.extend(ast);
+    }
+}
 
-    Fundable(String),
+fn endpoint_tokens(func: &Function) -> TokenStream {
+    todo!()
+}
 
-    Other(Attribute),
+fn fundable_tokens(func: &Function) -> TokenStream {
+    todo!()
+}
+
+fn before_fund_tokens(func: &Function) -> TokenStream {
+    todo!()
+}
+
+fn pure_tokens(func: &Function) -> TokenStream {
+    func.raw.to_token_stream()
+}
+
+pub fn func_attrs(func: &mut Function) -> Result<Vec<FuncAttribute>> {
+    todo!()
+}
+
+fn map_raw_attrs(func: &mut Function) -> Result<Vec<FuncAttribute>> {
+    let mut attrs = Vec::new();
+
+    for raw_attr in func.take_raw_attrs() {
+        // let attr: FuncAttribute = syn::parse2(raw_attr.tokens)?;
+    }
+
+    Ok(attrs)
 }
