91bc003befd1c40bc4c730afa332abb5611e8376
Yaron Wittenstein <yaron.wittenstein@gmail.com>
2020-11-09 17:01:09 +0200

    WIP: endpoint code generation.

--- crates/svm-sdk/macros/src/function.rs
+++ crates/svm-sdk/macros/src/function.rs
@@ -4,7 +4,9 @@ use proc_macro2::{Delimiter, Group, Ident, Literal, Punct, Spacing, Span, TokenS
 use quote::{quote, ToTokens};
 
 use syn::parse::{Parse, ParseStream};
-use syn::{Attribute, Error, FnArg, ItemFn, Pat, PatType, Result, ReturnType, Signature, Type};
+use syn::{
+    Attribute, Block, Error, FnArg, ItemFn, Pat, PatType, Result, ReturnType, Signature, Type,
+};
 
 use crate::{attr, FuncAttrKind, FuncAttribute};
 
@@ -27,6 +29,10 @@ impl Function {
 
     pub fn name(&self) -> Ident {
         self.raw_sig().ident.clone()
+    }
+
+    pub fn raw_body(&self) -> &Block {
+        &self.raw.block
     }
 
     pub fn raw_sig(&self) -> &Signature {
@@ -52,10 +58,6 @@ impl ToTokens for Function {
     }
 }
 
-fn pure_tokens(func: &Function) -> TokenStream {
-    func.raw.to_token_stream()
-}
-
 pub fn func_attrs(func: &mut Function) -> Result<Vec<FuncAttribute>> {
     let mut attrs = Vec::new();
 
@@ -73,6 +75,60 @@ fn rewrite_func(func: &mut Function) -> Result<TokenStream> {
 
     validate_attrs(&attrs)?;
 
+    let ast = if has_endpoint_attr(&attrs) {
+        expand_endpoint_attr(func, &attrs)?
+    } else if has_before_fund_attr(&attrs) {
+        expand_before_fund_attr(func, &attrs)?
+    } else {
+        expand_func(func, &attrs)?
+    };
+
+    let ast = expand_other_attrs(ast, &attrs)?;
+
+    Ok(ast)
+}
+
+fn validate_attrs(attrs: &[FuncAttribute]) -> Result<()> {
+    validate_attrs_no_dups(attrs)?;
+    validate_attrs_usage(attrs)?;
+    validate_attrs_order(attrs)?;
+
+    Ok(())
+}
+
+fn expand_endpoint_attr(func: &Function, attrs: &[FuncAttribute]) -> Result<TokenStream> {
+    debug_assert!(has_endpoint_attr(attrs));
+
+    let name = func.name();
+    let prologue = expand_endpoint_prologue(func)?;
+    let epilogue = expand_endpoint_epilogue(func)?;
+    let returns = expand_endpoint_returns(func)?;
+    let body = func.raw_body();
+
+    let ast = quote! {
+        #[no_mangle]
+        pub extern "C" fn #name() {
+
+            fn __inner__() #returns {
+                #prologue
+
+                #body
+            }
+
+            #epilogue
+        }
+    };
+
+    let ast = if has_fundable_attr(attrs) {
+        expand_fundable_attr(ast, &attrs)?
+    } else {
+        ast
+    };
+
+    Ok(ast)
+}
+
+fn expand_endpoint_prologue(func: &Function) -> Result<TokenStream> {
     let ast = quote! {
         //
     };
@@ -80,15 +136,57 @@ fn rewrite_func(func: &mut Function) -> Result<TokenStream> {
     Ok(ast)
 }
 
-fn validate_attrs(attrs: &[FuncAttribute]) -> Result<()> {
-    validate_no_dups(attrs)?;
-    validate_combinations(attrs)?;
-    validate_order(attrs)?;
+fn expand_endpoint_epilogue(func: &Function) -> Result<TokenStream> {
+    let ast = quote! {
+        //
+    };
 
-    Ok(())
+    Ok(ast)
 }
 
-fn validate_no_dups(attrs: &[FuncAttribute]) -> Result<()> {
+fn expand_endpoint_returns(func: &Function) -> Result<TokenStream> {
+    let ast = quote! {
+        //
+    };
+
+    Ok(ast)
+}
+
+fn expand_fundable_attr(ast: TokenStream, attrs: &[FuncAttribute]) -> Result<TokenStream> {
+    debug_assert!(has_fundable_attr(attrs));
+
+    let ast = quote! {
+        //
+    };
+
+    Ok(ast)
+}
+
+fn expand_before_fund_attr(func: &Function, attrs: &[FuncAttribute]) -> Result<TokenStream> {
+    debug_assert!(has_before_fund_attr(attrs));
+
+    let ast = quote! {
+        //
+    };
+
+    Ok(ast)
+}
+
+fn expand_other_attrs(ast: TokenStream, attrs: &[FuncAttribute]) -> Result<TokenStream> {
+    let ast = quote! {
+        //
+    };
+
+    Ok(ast)
+}
+
+fn expand_func(func: &Function, _attrs: &[FuncAttribute]) -> Result<TokenStream> {
+    let ast = func.raw.to_token_stream();
+
+    Ok(ast)
+}
+
+fn validate_attrs_no_dups(attrs: &[FuncAttribute]) -> Result<()> {
     let span = Span::call_site();
 
     let mut seen_endpoint = false;
@@ -131,7 +229,7 @@ fn validate_no_dups(attrs: &[FuncAttribute]) -> Result<()> {
     Ok(())
 }
 
-fn validate_combinations(attrs: &[FuncAttribute]) -> Result<()> {
+fn validate_attrs_usage(attrs: &[FuncAttribute]) -> Result<()> {
     let span = Span::call_site();
     let mut seen_endpoint = false;
     let mut seen_fundable = false;
@@ -170,7 +268,7 @@ fn validate_combinations(attrs: &[FuncAttribute]) -> Result<()> {
     Ok(())
 }
 
-fn validate_order(attrs: &[FuncAttribute]) -> Result<()> {
+fn validate_attrs_order(attrs: &[FuncAttribute]) -> Result<()> {
     let span = Span::call_site();
     let mut seen_endpoint = false;
     let mut seen_fundable = false;
@@ -196,15 +294,7 @@ fn validate_order(attrs: &[FuncAttribute]) -> Result<()> {
     Ok(())
 }
 
-fn expand_attr_endpoint(func: &Function) -> Result<TokenStream> {
-    let ast = quote! {
-        //
-    };
-
-    Ok(ast)
-}
-
-fn parse_endpoint_sig(func: &Function) -> Result<()> {
+fn validate_endpoint_sig(func: &Function) -> Result<()> {
     let sig = func.raw_sig();
     let span = Span::call_site();
 
@@ -255,7 +345,17 @@ fn parse_endpoint_sig(func: &Function) -> Result<()> {
 }
 
 fn validate_arg_pat(pat: &Box<Pat>) -> Result<()> {
-    Ok(())
+    match **pat {
+        Pat::Type(..) => Ok(()),
+        _ => {
+            let span = Span::call_site();
+
+            Err(Error::new(
+                span,
+                "`endpoint` parameters definitions are expected to be of pattern: `name: type`",
+            ))
+        }
+    }
 }
 
 fn validate_arg_type(ty: &Box<Type>) -> Result<()> {
@@ -276,23 +376,23 @@ fn validate_arg_type(ty: &Box<Type>) -> Result<()> {
         )),
         Type::Never(..) => Err(Error::new(
             span,
-            "`endpoint` can't use `!` on its parameters types",
+            "`endpoint` can't use `!` for its parameters types",
         )),
         Type::Paren(..) => Err(Error::new(
             span,
-            "`endpoint` can't use parentheses on its parameters types",
+            "`endpoint` can't use parentheses for its parameters types",
         )),
         Type::Ptr(..) => Err(Error::new(
             span,
-            "`endpoint` can't use raw pointers on its parameters types",
+            "`endpoint` can't use raw pointers for its parameters types",
         )),
         Type::Reference(..) => Err(Error::new(
             span,
-            "`endpoint` can't use reference on its parameters types",
+            "`endpoint` can't use references for its parameters types",
         )),
         Type::Slice(..) => Err(Error::new(
             span,
-            "`endpoint` can't use dynamically sized slices on its parameters types",
+            "`endpoint` can't use dynamically sized slices for its parameters types",
         )),
         Type::TraitObject(..) => Err(Error::new(
             span,
@@ -327,34 +427,48 @@ fn validate_ret_type(ty: &ReturnType) -> Result<()> {
                 )),
                 Type::Never(..) => Err(Error::new(
                     span,
-                    "`endpoint` can't use `!` on its parameters types",
+                    "`endpoint` can't use `!` for its parameters types",
                 )),
                 Type::Paren(..) => Err(Error::new(
                     span,
-                    "`endpoint` can't use parentheses on its parameters types",
+                    "`endpoint` can't use parentheses for its parameters types",
                 )),
                 Type::Ptr(..) => Err(Error::new(
                     span,
-                    "`endpoint` can't use raw pointers on its parameters types",
+                    "`endpoint` can't use raw pointers for its parameters types",
                 )),
                 Type::Reference(..) => Err(Error::new(
                     span,
-                    "`endpoint` can't use reference on its parameters types",
+                    "`endpoint` can't use reference for its parameters types",
                 )),
                 Type::Slice(..) => Err(Error::new(
                     span,
-                    "`endpoint` can't use dynamically sized slices on its parameters types",
+                    "`endpoint` can't use dynamically sized slices for its parameters types",
                 )),
                 Type::TraitObject(..) => Err(Error::new(
                     span,
                     "`endpoint` can't use trait objects for its parameters types",
                 )),
-                Type::Tuple(..) => Err(Error::new(
-                    span,
-                    "`endpoint` can't use right now tuples for its parameters types",
-                )),
                 _ => Ok(()),
             }
         }
     }
+}
+
+fn has_endpoint_attr(attrs: &[FuncAttribute]) -> bool {
+    attrs
+        .iter()
+        .any(|attr| matches!(attr, FuncAttribute::Endpoint))
+}
+
+fn has_before_fund_attr(attrs: &[FuncAttribute]) -> bool {
+    attrs
+        .iter()
+        .any(|attr| matches!(attr, FuncAttribute::BeforeFund))
+}
+
+fn has_fundable_attr(attrs: &[FuncAttribute]) -> bool {
+    attrs
+        .iter()
+        .any(|attr| matches!(attr, FuncAttribute::Fundable(..)))
 }
