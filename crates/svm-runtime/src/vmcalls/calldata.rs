d3d4d7b3cd337455257abeee73f792e03b3846b0
Yaron Wittenstein <yaron.wittenstein@gmail.com>
2020-12-03 16:43:52 +0200

    Wasmer bump `v1.0.0-beta1`

--- crates/svm-runtime/src/vmcalls/calldata.rs
+++ crates/svm-runtime/src/vmcalls/calldata.rs
@@ -1,19 +1,19 @@
 use crate::use_gas;
 use crate::Context;
 
-pub fn calldata_offset(ctx: &mut Context) -> i32 {
+pub fn calldata_offset(ctx: &Context) -> i32 {
     use_gas!("calldata_offset", ctx);
 
     calldata(ctx).0 as i32
 }
 
-pub fn calldata_len(ctx: &mut Context) -> i32 {
+pub fn calldata_len(ctx: &Context) -> i32 {
     use_gas!("calldata_len", ctx);
 
     calldata(ctx).1 as i32
 }
 
 #[inline]
-fn calldata(ctx: &mut Context) -> (usize, usize) {
+fn calldata(ctx: &Context) -> (usize, usize) {
     ctx.borrow().get_calldata()
 }
