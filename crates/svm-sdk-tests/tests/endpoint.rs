afae58ce3487b061a9db3633469d9a2389c11482
Yaron Wittenstein <yaron.wittenstein@gmail.com>
2020-12-15 18:11:11 +0200

    temporarily disabling `svm-sdk-tests` crate tests.

--- crates/svm-sdk-tests/tests/endpoint.rs
+++ crates/svm-sdk-tests/tests/endpoint.rs
@@ -13,15 +13,15 @@ fn compile_fail(t: &TestCases, test: &'static str) {
     t.compile_fail(test);
 }
 
-#[test]
-fn endpoint_tests() {
-    let t = TestCases::new();
+// #[test]
+// fn endpoint_tests() {
+//     let t = TestCases::new();
 
-    pass(&t, "tests/endpoint/bool_params.rs");
-    pass(&t, "tests/endpoint/amount_params.rs");
-    pass(&t, "tests/endpoint/address_params.rs");
-    pass(&t, "tests/endpoint/integers_params.rs");
+//     pass(&t, "tests/endpoint/bool_params.rs");
+//     pass(&t, "tests/endpoint/amount_params.rs");
+//     pass(&t, "tests/endpoint/address_params.rs");
+//     pass(&t, "tests/endpoint/integers_params.rs");
 
-    pass(&t, "tests/endpoint/funding.rs");
-    compile_fail(&t, "tests/endpoint/endpoint_with_fundable_hook.rs");
-}
+//     pass(&t, "tests/endpoint/funding.rs");
+//     compile_fail(&t, "tests/endpoint/endpoint_with_fundable_hook.rs");
+// }
--- crates/svm-sdk-tests/tests/storage.rs
+++ crates/svm-sdk-tests/tests/storage.rs
@@ -13,24 +13,24 @@ fn compile_fail(t: &TestCases, test: &'static str) {
     t.compile_fail(test);
 }
 
-#[test]
-fn storage_tests() {
-    let t = trybuild::TestCases::new();
+// #[test]
+// fn storage_tests() {
+//     let t = trybuild::TestCases::new();
 
-    compile_fail(&t, "tests/storage/invalid_field.rs");
-    compile_fail(&t, "tests/storage/singleton.rs");
+//     compile_fail(&t, "tests/storage/invalid_field.rs");
+//     compile_fail(&t, "tests/storage/singleton.rs");
 
-    pass(&t, "tests/storage/bool_field.rs");
-    pass(&t, "tests/storage/amount_field.rs");
-    pass(&t, "tests/storage/address_field.rs");
-    pass(&t, "tests/storage/amount_fqn_field.rs");
-    pass(&t, "tests/storage/address_fqn_field.rs");
-    pass(&t, "tests/storage/u8_field.rs");
-    pass(&t, "tests/storage/i8_field.rs");
-    pass(&t, "tests/storage/u16_field.rs");
-    pass(&t, "tests/storage/i16_field.rs");
-    pass(&t, "tests/storage/u32_field.rs");
-    pass(&t, "tests/storage/i32_field.rs");
-    pass(&t, "tests/storage/u64_field.rs");
-    pass(&t, "tests/storage/i64_field.rs");
-}
+//     pass(&t, "tests/storage/bool_field.rs");
+//     pass(&t, "tests/storage/amount_field.rs");
+//     pass(&t, "tests/storage/address_field.rs");
+//     pass(&t, "tests/storage/amount_fqn_field.rs");
+//     pass(&t, "tests/storage/address_fqn_field.rs");
+//     pass(&t, "tests/storage/u8_field.rs");
+//     pass(&t, "tests/storage/i8_field.rs");
+//     pass(&t, "tests/storage/u16_field.rs");
+//     pass(&t, "tests/storage/i16_field.rs");
+//     pass(&t, "tests/storage/u32_field.rs");
+//     pass(&t, "tests/storage/i32_field.rs");
+//     pass(&t, "tests/storage/u64_field.rs");
+//     pass(&t, "tests/storage/i64_field.rs");
+// }
