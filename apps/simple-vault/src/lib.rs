f502aa55b8e70dd6a7d11fd1a50540d4d1eb2cd5
Yaron Wittenstein <yaron.wittenstein@gmail.com>
2020-12-17 20:57:54 +0200

    WIP: implement the `simple-vault`.

--- apps/simple-vault/src/lib.rs
+++ apps/simple-vault/src/lib.rs
@@ -1,4 +1,4 @@
-use svm_sdk::app;
+use svm_sdk::{app, ensure};
 use svm_sdk::{Address, Amount};
 
 #[app]
@@ -28,8 +28,18 @@ mod App {
 
     #[fundable(default)]
     #[endpoint]
-    fn fund() {
-        //
+    fn withdraw(value: Amount, to: Address) {
+        do_auth();
+
+        let balance = Storage::get_balance();
+
+        ensure!(balance >= value, "`value` is larger than vault's `balance`");
+
+        let new_balance = balance - value;
+
+        Storage::set_balance(new_balance);
+
+        Node::transfer(&to, value);
     }
 
     #[fundable_hook]
@@ -38,5 +48,14 @@ mod App {
         let new = old + value;
 
         Storage::set_balance(new);
+    }
+
+    fn do_auth() {
+        // TODO: have `Node::public_key()`
+
+        let m = Address::from([0x10; Address::len()]);
+        let master = Storage::get_master();
+
+        ensure!(m == master, "wrong master key!");
     }
 }
