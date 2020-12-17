8ca8137042c07add579dfca22b5a201db46ce0f6
Yaron Wittenstein <yaron.wittenstein@gmail.com>
2020-12-17 15:36:51 +0200

    removing stale code. (#194)

--- apps/src/wallet/build.sh
+++
@@ -1,9 +0,0 @@
-cargo +nightly build --target wasm32-unknown-unknown --release
-cp target/wasm32-unknown-unknown/release/*.wasm ../../../apps/wasm
-
-# optimize the wasm artifact in order to reduce size
-pushd ../../wasm
-wasm2wat wallet.wasm > wallet.wast
-rm wallet.wasm
-wat2wasm wallet.wast
-popd
