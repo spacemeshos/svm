78226f4fff69760766bc8730e1c341e07f9c86fd
Filippo Costa <filippocosta.italy@gmail.com>
2021-07-12 11:50:10 +0200

    Fix crate name in doc comment

--- crates/query/src/lib.rs
+++ crates/query/src/lib.rs
@@ -1,4 +1,4 @@
-//! The purpose of this `svm-app-query` is to represent a schema for an `AppStorage`.
+//! The purpose of this `svm-query` crate is to represent a schema for an `AppStorage`.
 //! Consumers of the crate will be SVM clients that want to give a better user-experience to its end-users.
 //!
 //! The obvious usage use-case is a UX Wallet that needs to render the App's current state in a friendly way.
