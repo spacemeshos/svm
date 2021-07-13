435d98fac31a0b93f5443c807c65218bfa623e12
Filippo Costa <filippocosta.italy@gmail.com>
2021-07-13 16:17:41 +0200

    Refactor the new_store utility function

--- crates/runtime/src/wasm_store.rs
+++ crates/runtime/src/wasm_store.rs
@@ -1,15 +1,18 @@
-use wasmer::{Cranelift, Store, Universal};
+use wasmer::Store;
 
 /// New fresh `Store`.
 #[must_use]
 pub fn new_store() -> Store {
-    //#[cfg(feature, "default-cranelift")]
-    new_store_cranelift()
-}
+    // By importing items within `if` blocks rather than at the top of the file,
+    // we don't run the risk of importing stuff that specific features don't
+    // use (which would cause compiler warnings).
+    if cfg!(feature = "default-cranelift") {
+        use wasmer::{Cranelift, Universal};
 
-//#[cfg(feature, "default-cranelift")]
-fn new_store_cranelift() -> Store {
-    let compiler_config = Cranelift::default();
-    let engine = Universal::new(compiler_config).engine();
-    Store::new(&engine)
+        let compiler_config = Cranelift::default();
+        let engine = Universal::new(compiler_config).engine();
+        Store::new(&engine)
+    } else {
+        panic!("No Wasmer backend available.");
+    }
 }
