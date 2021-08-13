b5bde7ea1aff7ea12b04eb04d3d18a352e2390bf
Filippo Costa <filippo@neysofu.me>
2021-08-13 15:09:22 +0200

    Empty lines in enum

--- crates/state/src/error.rs
+++ crates/state/src/error.rs
@@ -6,14 +6,18 @@ pub type Result<T> = std::result::Result<T, GlobalStateError>;
 pub enum GlobalStateError {
     #[error("Invalid item.")]
     InvalidItem,
+
     #[error("Please commit dirty changes or rollback to avoid data loss.")]
     DirtyChanges,
+
     #[error("Hash collision. Make sure that you're not using the hash of a known item.")]
     Collision,
+
     #[error(
         "Possible self-reference at the database layer. We couldn't reach the leaf fast enough."
     )]
     Cyclic,
+
     #[error("SQLite error.")]
     Sqlite(#[from] sqlx::Error),
 }
