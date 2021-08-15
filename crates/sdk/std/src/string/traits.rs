a89e019a5b70006a302ced6bb2f5aafaffa8083a
Yaron Wittenstein <yaron.wittenstein@gmail.com>
2021-08-15 15:50:28 +0300

    Implementing number to string

--- crates/sdk/std/src/string/traits.rs
+++ crates/sdk/std/src/string/traits.rs
@@ -24,16 +24,16 @@ pub struct HexDigit(u8);
 
 impl ToToken for HexDigit {
     fn to_token(&self) -> Token {
-        let byte = self.0;
-        debug_assert!(byte < 16);
+        let value = self.0;
+        debug_assert!(value < 16);
 
-        if byte < 10 {
-            let digit = DecDigit(byte);
+        if value < 10 {
+            let digit = DecDigit(value);
             return digit.to_token();
         }
 
         // TODO: is that possible to use `seq-macro` here?
-        match byte {
+        match value {
             10 => Token::One(b'A'),
             11 => Token::One(b'B'),
             12 => Token::One(b'C'),
@@ -47,11 +47,11 @@ impl ToToken for HexDigit {
 
 impl ToToken for DecDigit {
     fn to_token(&self) -> Token {
-        let byte = self.0;
-        debug_assert!(byte < 10);
+        let value = self.0;
+        debug_assert!(value < 10);
 
         // TODO: is that possible to use `seq-macro` here?
-        match byte {
+        match value {
             0 => Token::One(b'0'),
             1 => Token::One(b'1'),
             2 => Token::One(b'2'),
@@ -67,73 +67,113 @@ impl ToToken for DecDigit {
     }
 }
 
+#[inline]
+fn next_digit(value: u64) -> (u8, u64, bool) {
+    let digit = value % 10;
+    let value = value / 10;
+    let completed = value == 0;
+
+    (digit as u8, value, completed)
+}
+
+fn num_as_string(num: u64, is_negative: bool) -> String {
+    let mut value = num;
+    let mut digits = [0u8; 20];
+    let mut has_more = true;
+    let mut used_count = 0;
+
+    seq_macro::seq!(N in 0..21 {
+        if has_more {
+            let (digit, new_value, completed) = next_digit(value);
+
+            digits[used_count] = digit;
+            used_count += 1;
+
+            value = new_value;
+            has_more = !completed;
+        }
+        else {
+            return concat_digits(&digits, used_count, is_negative);
+            debug_assert_eq!(value, 0);
+        }
+    });
+
+    // we should never get here
+    crate::panic()
+}
+
+fn concat_digits(digits: &[u8; 20], used_count: usize, is_negative: bool) -> String {
+    let mut sb = StringBuilder::with_capacity(21);
+    if is_negative {
+        sb.push_token(Token::One(b'-'));
+    }
+
+    seq_macro::seq!(N in 0..20 {
+        if N < used_count {
+            let digit = digits[N];
+            let token = DecDigit(digit).to_token();
+            sb.push_token(token);
+        }
+        else {
+            return sb.build()
+        }
+    });
+
+    sb.build()
+}
+
 impl ToString for u8 {
     fn to_string(&self) -> String {
-        let mut sb = StringBuilder::with_capacity(2);
-        let byte = *self;
-
-        if byte < 10 {
-            let digit = DecDigit(byte);
-            sb.push_token(digit.to_token());
-        } else {
-            let left = (byte & 0xF0) >> 4;
-            let right = byte & 0x0F;
-
-            let left = DecDigit(left).to_token();
-            let right = DecDigit(right).to_token();
-
-            sb.push_token(left);
-            sb.push_token(right);
-        }
-
-        sb.build()
+        num_as_string(*self as u64, false)
     }
 }
 
 impl ToString for i8 {
     fn to_string(&self) -> String {
-        let mut sb = StringBuilder::with_capacity(2);
-        let mut byte = *self;
-
-        if byte < 0 {
-            sb.push_token(Token::One(b'-'));
-            byte = -(byte);
-        }
-        debug_assert!(byte >= 0);
-
-        (byte as u8).to_string()
+        let num = self.abs();
+        let is_negative = *self < 0;
+        num_as_string(num as u64, is_negative)
     }
 }
 
 impl ToString for u16 {
     fn to_string(&self) -> String {
-        todo!()
-    }
-}
-
-impl ToString for u32 {
-    fn to_string(&self) -> String {
-        todo!()
-    }
-}
-
-impl ToString for u64 {
-    fn to_string(&self) -> String {
-        todo!()
+        num_as_string(*self as u64, false)
     }
 }
 
 impl ToString for i16 {
     fn to_string(&self) -> String {
-        let mut sb = StringBuilder::with_capacity(4);
-        let mut num = *self;
+        let num = self.abs();
+        let is_negative = *self < 0;
+        num_as_string(num as u64, is_negative)
+    }
+}
 
-        if num < 0 {
-            sb.push_token(Token::One(b'-'));
-            num = -(num);
-        }
-        debug_assert!(num >= 0);
+impl ToString for u32 {
+    fn to_string(&self) -> String {
+        num_as_string(*self as u64, false)
+    }
+}
 
-        (num as u16).to_string()
+impl ToString for i32 {
+    fn to_string(&self) -> String {
+        let num = self.abs();
+        let is_negative = *self < 0;
+        num_as_string(num as u64, is_negative)
+    }
+}
+
+impl ToString for u64 {
+    fn to_string(&self) -> String {
+        num_as_string(*self as u64, false)
+    }
+}
+
+impl ToString for i64 {
+    fn to_string(&self) -> String {
+        let num = self.abs();
+        let is_negative = *self < 0;
+        num_as_string(num as u64, is_negative)
     }
 }
