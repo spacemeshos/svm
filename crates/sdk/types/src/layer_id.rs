1a8d4cf4077f24357fdd31d186e1a59e1be74fc2
Yaron Wittenstein <yaron.wittenstein@gmail.com>
2021-08-16 14:59:28 +0300

    implemtenting `ToString` for `LayerId`

--- crates/sdk/types/src/layer_id.rs
+++ crates/sdk/types/src/layer_id.rs
@@ -1,3 +1,5 @@
+use svm_sdk_std::{String, StringBuilder, ToString, Token};
+
 /// Represents a `Layer`
 #[derive(PartialEq, Copy, Clone)]
 #[repr(transparent)]
@@ -7,5 +9,35 @@ pub struct LayerId(pub u64);
 impl core::fmt::Debug for LayerId {
     fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
         write!(f, "LayerId({})", self.0)
+    }
+}
+
+impl ToString for LayerId {
+    fn to_string(&self) -> String {
+        let mut sb = StringBuilder::with_capacity(20 + " coins".len());
+
+        let s = self.0.to_string();
+        sb.push_str(&String::new("[Layer "));
+        sb.push_str(&s);
+        sb.push_token(Token::One(b']'));
+        sb.build()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn layer_to_string() {
+        extern crate std;
+
+        let layer = LayerId(123);
+        let string = svm_sdk_std::ToString::to_string(&layer);
+
+        let vec: std::vec::Vec<u8> = string.as_bytes().into();
+        let string = unsafe { std::string::String::from_utf8_unchecked(vec) };
+
+        assert_eq!(string.as_str(), "[Layer 123]");
     }
 }
