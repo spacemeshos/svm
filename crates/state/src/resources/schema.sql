0418356277ccdd984b5ebbdf855e72d4946ef11f
Filippo Costa <filippo@neysofu.me>
2021-08-16 14:00:00 +0200

    Don't delete existing data in SQLite instance

--- crates/state/src/resources/schema.sql
+++ crates/state/src/resources/schema.sql
@@ -1,13 +1,11 @@
 BEGIN TRANSACTION;
 
-DROP TABLE IF EXISTS "commits";
 CREATE TABLE IF NOT EXISTS "commits" (
 	"id" INTEGER NOT NULL UNIQUE,
 	"fingerprint" BLOB NOT NULL CHECK(length("fingerprint") == 32),
 	PRIMARY KEY("id")
 );
 
-DROP TABLE IF EXISTS "values";
 CREATE TABLE IF NOT EXISTS "values" (
 	"id" INTEGER NOT NULL UNIQUE,
 	"commit_id"	INTEGER,
@@ -17,12 +15,10 @@ CREATE TABLE IF NOT EXISTS "values" (
 	FOREIGN KEY("commit_id") REFERENCES "commits"("id")
 );
 
-DROP INDEX IF EXISTS "commits_by_fingerprint";
 CREATE INDEX IF NOT EXISTS "commits_by_fingerprint" ON "commits" (
 	"fingerprint" ASC
 );
 
-DROP INDEX IF EXISTS "values_by_commit_id_and_key_hash";
 CREATE UNIQUE INDEX IF NOT EXISTS "values_by_commit_id_and_key_hash" ON "values" (
 	"commit_id" ASC,
 	"key_hash" ASC
