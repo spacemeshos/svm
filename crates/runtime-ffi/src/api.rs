cbcd75e5f69e9b1de9a38f85f55be9f5a0350d19
Filippo Costa <filippocosta.italy@gmail.com>
2021-09-13 21:48:17 +0200

    Better handling of runtime pointers

--- crates/runtime-ffi/src/api.rs
+++ crates/runtime-ffi/src/api.rs
@@ -4,7 +4,7 @@ use std::slice;
 use std::{ffi::c_void, panic::UnwindSafe};
 
 use svm_codec::Codec;
-use svm_runtime::{Runtime, ValidateError};
+use svm_runtime::{DefaultRuntime, Runtime, ValidateError};
 use svm_types::{Address, BytesPrimitive, Context, Envelope, Layer, TemplateAddr};
 
 use crate::svm_result_t;
@@ -42,10 +42,8 @@ pub unsafe extern "C" fn svm_memory_runtime_create(runtime: *mut *mut c_void) ->
     catch_unwind_or_fail(|| {
         debug!("`svm_memory_runtime_create` start");
 
-        let boxed: Box<dyn Runtime> = Box::new(create_memory_runtime());
-        unsafe { runtime.as_uninit_mut() }
-            .unwrap()
-            .write(Box::leak(Box::new(boxed)) as *mut _ as *mut c_void);
+        let boxed = Box::new(create_memory_runtime());
+        *runtime = Box::leak(boxed) as *mut _ as *mut c_void;
 
         debug!("`svm_memory_runtime_create` end");
 
@@ -71,8 +69,7 @@ pub unsafe extern "C" fn svm_memory_runtime_create(runtime: *mut *mut c_void) ->
 #[must_use]
 #[no_mangle]
 pub unsafe extern "C" fn svm_runtime_destroy(runtime: *mut c_void) -> svm_result_t {
-    let _ = Box::from_raw(runtime.cast::<Box<dyn Runtime>>());
-
+    let _ = Box::<DefaultRuntime>::from_raw(runtime.cast());
     svm_result_t::OK
 }
 
@@ -107,7 +104,7 @@ pub unsafe extern "C" fn svm_validate_deploy(
         runtime,
         message,
         message_size,
-        |r, m| Runtime::validate_deploy(&mut *r, m),
+        |r, m| Runtime::validate_deploy(r, m),
         "svm_validate_deploy",
     )
 }
@@ -143,7 +140,7 @@ pub unsafe extern "C" fn svm_validate_spawn(
         runtime,
         message,
         message_size,
-        |r, m| r.validate_spawn(m),
+        |r, m| Runtime::validate_spawn(r, m),
         "svm_validate_spawn",
     )
 }
@@ -175,7 +172,7 @@ pub unsafe extern "C" fn svm_validate_call(
         runtime,
         message,
         message_size,
-        |r, m| r.validate_call(m),
+        |r, m| Runtime::validate_call(r, m),
         "svm_validate_call",
     )
 }
@@ -221,7 +218,7 @@ pub unsafe extern "C" fn svm_deploy(
         message,
         message_size,
         context,
-        |r, e, m, c| r.deploy(e, m, c),
+        |r, e, m, c| Runtime::deploy(r, e, m, c),
         "svm_deploy",
     )
 }
@@ -373,8 +370,7 @@ pub unsafe extern "C" fn svm_call(
 #[no_mangle]
 pub unsafe extern "C" fn svm_rewind(runtime: *mut c_void, layer_id: u64) -> svm_result_t {
     catch_unwind_or_fail(|| {
-        let runtime = runtime.cast::<Box<dyn Runtime>>().as_mut().unwrap();
-        runtime.rewind(Layer(layer_id))?;
+        runtime_mut(runtime).rewind(Layer(layer_id))?;
         svm_result_t::OK
     })
 }
@@ -383,8 +379,7 @@ pub unsafe extern "C" fn svm_rewind(runtime: *mut c_void, layer_id: u64) -> svm_
 #[no_mangle]
 pub unsafe extern "C" fn svm_commit(runtime: *mut c_void) -> svm_result_t {
     catch_unwind_or_fail(|| {
-        let runtime = runtime.cast::<Box<dyn Runtime>>().as_mut().unwrap();
-        runtime.commit()?;
+        runtime_mut(runtime).commit()?;
         svm_result_t::OK
     })
 }
@@ -400,7 +395,7 @@ pub unsafe extern "C" fn svm_get_account(
     template_addr: *mut u8,
 ) -> svm_result_t {
     catch_unwind_or_fail(|| {
-        let runtime = runtime.cast::<Box<dyn Runtime>>().as_mut().unwrap();
+        let runtime = runtime_mut(runtime);
         let account_addr = Address::new(std::slice::from_raw_parts(account_addr, Address::N));
         let template_addr = std::slice::from_raw_parts_mut(template_addr, TemplateAddr::N);
         let account_data = runtime.get_account(&account_addr).unwrap();
@@ -424,20 +419,20 @@ unsafe fn svm_runtime_action<F, C>(
     f_name: &str,
 ) -> svm_result_t
 where
-    F: FnOnce(&mut dyn Runtime, &Envelope, &[u8], &Context) -> C + UnwindSafe,
+    F: FnOnce(&mut DefaultRuntime, &Envelope, &[u8], &Context) -> C + UnwindSafe,
     C: Codec + UnwindSafe + std::fmt::Debug,
 {
     catch_unwind_or_fail(|| {
+        let runtime = runtime_mut(runtime);
         let message = slice::from_raw_parts(message, message_size as usize);
         let envelope = slice::from_raw_parts(envelope, Envelope::fixed_size().unwrap());
         let context = slice::from_raw_parts(context, Context::fixed_size().unwrap());
-        let runtime = runtime.cast::<Box<dyn Runtime>>().as_mut().unwrap();
 
         debug!("`{}` start", f_name);
 
         let envelope = Envelope::decode_bytes(envelope)?;
         let context = Context::decode_bytes(context)?;
-        let receipt = f(&mut **runtime, &envelope, &message, &context);
+        let receipt = f(runtime, &envelope, &message, &context);
 
         debug!("`{}` returns `SVM_SUCCESS`", f_name);
         svm_result_t::new_receipt(&receipt.encode_to_vec())
@@ -445,20 +440,20 @@ where
 }
 
 unsafe fn svm_validate<F>(
-    runtime: *mut c_void,
+    runtime_ptr: *mut c_void,
     message: *const u8,
     message_size: u32,
     validate_f: F,
     f_name: &str,
 ) -> svm_result_t
 where
-    F: FnOnce(&mut dyn Runtime, &[u8]) -> Result<(), ValidateError> + UnwindSafe,
+    F: FnOnce(&mut DefaultRuntime, &[u8]) -> Result<(), ValidateError> + UnwindSafe,
 {
     catch_unwind_or_fail(|| {
-        let runtime = runtime.cast::<Box<dyn Runtime>>().as_mut().unwrap();
+        let runtime = runtime_mut(runtime_ptr);
         let message = slice::from_raw_parts(message, message_size as usize);
 
-        match validate_f(&mut **runtime, message) {
+        match validate_f(runtime, message) {
             Ok(()) => {
                 debug!("`{}` returns `SVM_SUCCESS`", f_name);
                 svm_result_t::OK
@@ -490,4 +485,8 @@ Panic information: {:?}
             .as_bytes(),
         )
     })
+}
+
+unsafe fn runtime_mut<'a>(ptr: *mut c_void) -> &'a mut DefaultRuntime {
+    Box::leak(Box::<DefaultRuntime>::from_raw(ptr.cast()))
 }
