aeef8952f7d471621435de3ea1df1cbf5b3e1ef8
Filippo Costa <filippocosta.italy@gmail.com>
2021-11-01 16:08:23 +0100

    Remove chmod +x svm-cli

--- tools/cp-artifacts.sh
+++ tools/cp-artifacts.sh
@@ -10,17 +10,14 @@ if [[ "${OS}" == "windows-latest" ]]; then
     cp target/${TARGET}/release/svm_runtime_ffi.pdb bins/svm.pdb
     cp target/${TARGET}/release/svm-cli.exe bins/svm-cli.exe
     cp target/${TARGET}/release/svm.h bins/svm.h
-    chmod +x bins/svm-cli.exe
 elif [[ "${OS}" == "macos-latest" ]]; then
     cp target/${TARGET}/release/libsvm_runtime_ffi.dylib bins/libsvm.dylib
     cp target/${TARGET}/release/libsvm_runtime_ffi.a bins/libsvm.a
     cp target/${TARGET}/release/svm-cli bins/svm-cli
     cp target/${TARGET}/release/svm.h bins/svm.h
-    chmod +x bins/svm-cli
 elif [[ "${OS}" == "ubuntu-latest" ]]; then
     cp target/${TARGET}/release/libsvm_runtime_ffi.so bins/libsvm.so
     cp target/${TARGET}/release/libsvm_runtime_ffi.a bins/libsvm.a
     cp target/${TARGET}/release/svm-cli bins/svm-cli
     cp target/${TARGET}/release/svm.h bins/svm.h
-    chmod +x bins/svm-cli
 fi
