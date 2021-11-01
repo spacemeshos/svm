6a0badbcf5d5d4d6b0de021d1d9762a7c20e7607
Filippo Costa <filippocosta.italy@gmail.com>
2021-11-01 16:44:19 +0100

    Move cp-artifacts.sh into .github directory

---
+++ .github/copy-artifacts.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -e
+
+mkdir -p bins
+
+if [[ "${OS}" == "windows-latest" ]]; then
+    cp target/release/svm_runtime_ffi.dll bins/svm.dll
+    cp target/release/svm_runtime_ffi.lib bins/svm.lib
+    cp target/release/svm_runtime_ffi.pdb bins/svm.pdb
+    cp target/release/svm-cli.exe bins/svm-cli.exe
+    cp target/release/svm.h bins/svm.h
+elif [[ "${OS}" == "macos-latest" ]]; then
+    cp target/release/libsvm_runtime_ffi.dylib bins/libsvm.dylib
+    cp target/release/libsvm_runtime_ffi.a bins/libsvm.a
+    cp target/release/svm-cli bins/svm-cli
+    cp target/release/svm.h bins/svm.h
+elif [[ "${OS}" == "ubuntu-latest" ]]; then
+    cp target/release/libsvm_runtime_ffi.so bins/libsvm.so
+    cp target/release/libsvm_runtime_ffi.a bins/libsvm.a
+    cp target/release/svm-cli bins/svm-cli
+    cp target/release/svm.h bins/svm.h
+fi
