ecddb82402bbe01630875e9ed173f34cb00f860c
Yaron Wittenstein <yaron.wittenstein@gmail.com>
2021-12-02 11:18:42 +0200

    Wasmer update to v2.1.0 (Singlepass on Windows!)

--- crates/runtime/src/wasm_store.rs
+++ crates/runtime/src/wasm_store.rs
@@ -1,21 +1,8 @@
-use wasmer::Store;
+use wasmer::{Singlepass, Store, Universal};
 
 /// New fresh `Store`.
-#[cfg(feature = "default-cranelift")]
 #[must_use]
 pub fn new_store() -> Store {
-    use wasmer::{Cranelift, Universal};
-
-    let engine = Universal::new(Cranelift::default()).engine();
-    Store::new(&engine)
-}
-
-/// New fresh `Store`.
-#[cfg(feature = "default-singlepass")]
-#[must_use]
-pub fn new_store() -> Store {
-    use wasmer::{Singlepass, Universal};
-
     let engine = Universal::new(Singlepass::default()).engine();
     Store::new(&engine)
 }
