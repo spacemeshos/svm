056d2fd07dfe3858abe933c7a1580bcc12bc3bd0
Filippo Costa <filippocosta.italy@gmail.com>
2022-01-21 11:20:52 +0100

    Copy aarch64 macOS artifacts

--- .github/copy-artifacts.sh
+++ .github/copy-artifacts.sh
@@ -11,9 +11,12 @@ if [[ "${OS}" == "windows-latest" ]]; then
     cp target/release/svm-cli.exe bins/svm-cli.exe
     cp target/release/svm.h bins/svm.h
 elif [[ "${OS}" == "macos-latest" ]]; then
-    cp target/release/libsvm_runtime_ffi.dylib bins/libsvm.dylib
-    cp target/release/libsvm_runtime_ffi.a bins/libsvm.a
-    cp target/release/svm-cli bins/svm-cli
+    cp target/aarch64-apple-darwin/release/libsvm_runtime_ffi.dylib bins/x86_64/libsvm.dylib
+    cp target/aarch64-apple-darwin/release/libsvm_runtime_ffi.a bins/x86_64/libsvm.a
+    cp target/aarch64-apple-darwin/release/svm-cli bins/x86_64/svm-cli
+    cp target/release/libsvm_runtime_ffi.dylib bins/x86_64/libsvm.dylib
+    cp target/release/libsvm_runtime_ffi.a bins/x86_64/libsvm.a
+    cp target/release/svm-cli bins/x86_64/svm-cli
     cp target/release/svm.h bins/svm.h
 elif [[ "${OS}" == "ubuntu-latest" ]]; then
     cp target/release/libsvm_runtime_ffi.so bins/libsvm.so
