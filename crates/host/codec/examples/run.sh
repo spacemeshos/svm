1ab65fa4206f711d4f3dce78cb22bc368fe22e77
Filippo Costa <filippocosta.italy@gmail.com>
2022-02-15 16:05:39 +0100

    Fix codec build

--- crates/host/codec/examples/run.sh
+++ crates/host/codec/examples/run.sh
@@ -1,5 +1,5 @@
 ./../build.sh 
-cp ../../../target/wasm32-unknown-unknown/release/svm_codec.wasm svm_codec.wasm
+cp ../../../../target/wasm32-unknown-unknown/release/svm_codec.wasm svm_codec.wasm
 
 npm install
 npm test
